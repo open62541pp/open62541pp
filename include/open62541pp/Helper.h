a2a3aa50b9b31bb94d287c1d20622814c6cfd880
Lukas Berbuer <lukas.berbuer@gmail.com>
2019-09-29 19:37:53 +0200

    Add generic TypeWrapper class
    Implement wrapper for String, Guid, ByteString, QualifiedName, LocalizedText

--- include/open62541pp/Helper.h
+++ include/open62541pp/Helper.h
@@ -1,59 +1,25 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 
 #include "open62541/types.h"
 #include "open62541/types_generated_handling.h"
 
 namespace opcua {
 
-inline std::string uaStringToString(UA_String input) noexcept {
+inline std::string uaStringToString(const UA_String& input) noexcept {
     if (input.data == nullptr)
         return {};
     
     return std::string((const char*) input.data, input.length); // NOLINT
 }
 
-class QualifiedName {
-public:
-    QualifiedName(uint16_t namespaceIndex, std::string_view name)
-        : name_(UA_QUALIFIEDNAME_ALLOC(namespaceIndex, name.data())) {}
-
-    ~QualifiedName() { UA_QualifiedName_deleteMembers(&name_); }
-
-    QualifiedName(const QualifiedName& other)            { UA_QualifiedName_copy(&other.name_, &name_); }
-    QualifiedName& operator=(const QualifiedName& other) { UA_QualifiedName_copy(&other.name_, &name_); return *this; }
-
-    inline bool operator==(const QualifiedName& other) const { return UA_QualifiedName_equal(&name_, other.handle()); }
-    inline bool operator!=(const QualifiedName& other) const { return !operator==(other); }
-
-    inline uint16_t    getNamespaceIndex() const { return name_.namespaceIndex; } 
-    inline std::string getName()           const { return uaStringToString(name_.name); } 
-
-    inline       UA_QualifiedName* handle()       { return &name_; }
-    inline const UA_QualifiedName* handle() const { return &name_; }
-private:
-    UA_QualifiedName name_;
-};
-
-
-class LocalizedText {
-public:
-    LocalizedText(std::string_view text, std::string_view locale = "")
-        : text_(UA_LOCALIZEDTEXT_ALLOC(locale.data(), text.data())) {}
-
-    ~LocalizedText() { UA_LocalizedText_deleteMembers(&text_); }
-
-    LocalizedText(const LocalizedText& other)            { UA_LocalizedText_copy(&other.text_, &text_); }
-    LocalizedText& operator=(const LocalizedText& other) { UA_LocalizedText_copy(&other.text_, &text_); return *this; }
-
-    inline std::string getText()   const { return uaStringToString(text_.text); } 
-    inline std::string getLocale() const { return uaStringToString(text_.locale); } 
-
-    inline       UA_LocalizedText* handle()       { return &text_; }
-    inline const UA_LocalizedText* handle() const { return &text_; }
-private:
-    UA_LocalizedText text_;
-};
+inline std::string_view uaStringToStringView(const UA_String& input) noexcept {
+    if (input.data == nullptr)
+        return {};
+    
+    return std::string_view((const char*) input.data, input.length); // NOLINT
+}
 
 } // namespace opcua
---
+++ include/open62541pp/TypeWrapper.h
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <array>
+#include <algorithm> // copy
+
+#include "open62541pp/Types.h"
+#include "open62541pp/Helper.h"
+#include "open62541pp/ErrorHandling.h"
+
+namespace opcua {
+
+/**
+ * Template base class to wrap UA_* type objects.
+ * 
+ * The derived classes should implement specific constructors to convert from other data types.
+ */
+template <typename T, Type type = getType<T>()>
+class TypeWrapper {
+public:
+    using BaseClass = TypeWrapper<T, type>;
+
+    TypeWrapper() { init(); }
+
+    /// Constructor with native UA_* type (deep copy)
+    explicit TypeWrapper(const T& data) { init(); copy(data); }
+    /// Constructor with native UA_* type (move rvalue)
+    explicit TypeWrapper(T&& data) : data_(data) {}
+
+    virtual ~TypeWrapper() { clear(); };
+
+    /// Copy constructor (deep copy)
+    TypeWrapper(const TypeWrapper& other)            { clear(); copy(other.data_); }
+    /// Copy assignment (deep copy)
+    TypeWrapper& operator=(const TypeWrapper& other) { clear(); copy(other.data_); return *this; }
+
+    /// Return type enum
+    inline Type               getType()     const { return type; }
+    /// Return UA_DataType object
+    inline const UA_DataType* getDataType() const { return getUaDataType(type); }
+
+    /// Return pointer to wrapped UA data type
+    inline T*       handle()       { return &data_; }
+    /// Return const pointer to wrapped UA data type
+    inline const T* handle() const { return &data_; };
+protected:
+    inline void init()  { UA_init(&data_, getDataType()); }
+    inline void clear() { UA_clear(&data_, getDataType()); }
+    
+    /// Deep copy of data
+    inline void copy(const T& data) {
+        auto status = UA_copy(&data, &data_, getDataType());
+        checkStatusCodeException(status);
+    }
+
+    T data_ {};
+};
+
+
+/**
+ * UA_String wrapper class
+ */
+class String : public TypeWrapper<UA_String> {
+public:
+    using BaseClass::BaseClass; // inherit contructors
+
+    explicit String(std::string_view str) {
+        data_ = UA_STRING_ALLOC(str.data());
+    }
+
+    inline bool operator==(const String& other) const { return UA_String_equal(&data_, other.handle()); }
+    inline bool operator!=(const String& other) const { return !operator==(other); }
+
+    inline std::string get() const { return uaStringToString(data_); }
+};
+
+
+/**
+ * UA_Guid wrapper class
+ */
+class Guid : public TypeWrapper<UA_Guid> {
+public:
+    using BaseClass::BaseClass; // inherit contructors
+
+    Guid(UA_UInt32 data1, UA_UInt16 data2, UA_UInt16 data3, std::array<UA_Byte, 8> data4) {
+        data_.data1 = data1; 
+        data_.data2 = data2;
+        data_.data3 = data3;
+        std::copy(data4.begin(), data4.end(), data_.data4);
+    }
+
+    inline bool operator==(const Guid& other) const { return UA_Guid_equal(&data_, other.handle()); }
+    inline bool operator!=(const Guid& other) const { return !operator==(other); }
+};
+
+
+/**
+ * UA_ByteString wrapper class
+ * 
+ * UA_ByteString is a typedef of UA_String. Therefore the type can not be deduced from UA_ByteString itself.
+ * Second template argument (Type::ByteString) necessary to map to the right type.
+ */
+class ByteString : public TypeWrapper<UA_ByteString, Type::ByteString> {
+public:
+    using BaseClass::BaseClass; // inherit contructors
+
+    explicit ByteString(std::string_view str) {
+        data_ = UA_BYTESTRING_ALLOC(str.data());
+    }
+
+    inline bool operator==(const ByteString& other) const { return UA_ByteString_equal(&data_, other.handle()); }
+    inline bool operator!=(const ByteString& other) const { return !operator==(other); }
+
+    inline std::string get() const { return uaStringToString(data_); }
+};
+
+
+/**
+ * UA_QualifiedName wrapper class
+ */
+class QualifiedName : public TypeWrapper<UA_QualifiedName> {
+public:
+    using BaseClass::BaseClass; // inherit contructors
+
+    QualifiedName(uint16_t namespaceIndex, std::string_view name) {
+        data_ = UA_QUALIFIEDNAME_ALLOC(namespaceIndex, name.data());
+    }
+
+    inline bool operator==(const QualifiedName& other) const { return UA_QualifiedName_equal(&data_, other.handle()); }
+    inline bool operator!=(const QualifiedName& other) const { return !operator==(other); }
+
+    inline uint16_t    getNamespaceIndex() const { return data_.namespaceIndex; } 
+    inline std::string getName()           const { return uaStringToString(data_.name); } 
+};
+
+
+/**
+ * UA_LocalizedText wrapper class
+ */
+class LocalizedText : public TypeWrapper<UA_LocalizedText> {
+public:
+    using BaseClass::BaseClass; // inherit contructors
+
+    LocalizedText(std::string_view text, std::string_view locale = "") {
+        data_ = UA_LOCALIZEDTEXT_ALLOC(locale.data(), text.data());
+    }
+
+    inline std::string getText()   const { return uaStringToString(data_.text); } 
+    inline std::string getLocale() const { return uaStringToString(data_.locale); } 
+};
+
+} // namespace opcua
