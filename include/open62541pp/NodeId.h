9192cf775ec38b0a814075e411acafb6878837f9
Lukas Berbuer <lukas.berbuer@gmail.com>
2019-09-30 10:21:24 +0200

    Base NodeId on TypeWrapper and add constructors

--- include/open62541pp/NodeId.h
+++ include/open62541pp/NodeId.h
@@ -1,45 +1,68 @@
 #pragma once
 
 #include <string_view>
+#include <variant>
 
 #include "open62541/types.h"
 #include "open62541/types_generated_handling.h"
 
+#include "open62541pp/TypeWrapper.h"
+
 namespace opcua {
 
-class NodeId {
+class NodeId : public TypeWrapper<UA_NodeId> {
 public:
-    NodeId(uint32_t identifier, uint16_t namespaceIndex = 0)
-        : id_(UA_NODEID_NUMERIC(namespaceIndex, identifier)) {}
+    using BaseClass::BaseClass; // inherit contructors
 
-    NodeId(std::string_view identifier, uint16_t namespaceIndex = 0)
-        : id_(UA_NODEID_STRING_ALLOC(namespaceIndex, identifier.data())) {}
-
-    NodeId(const UA_NodeId& id) {
-        UA_NodeId_copy(&id, &id_);
+    /// Create NodeId with numeric identifier
+    NodeId(UA_UInt32 identifier, UA_UInt16 namespaceIndex = 0) {
+        data_ = UA_NODEID_NUMERIC(namespaceIndex, identifier);
     }
 
-    ~NodeId() { UA_NodeId_deleteMembers(&id_); }
+    /// Create NodeId with String identifier from standard strings
+    NodeId(std::string_view identifier, UA_UInt16 namespaceIndex = 0) {
+        data_ = UA_NODEID_STRING_ALLOC(namespaceIndex, identifier.data());
+    }
 
-    NodeId(const NodeId& other)            { UA_NodeId_copy(&other.id_, &id_); }
-    NodeId& operator=(const NodeId& other) { UA_NodeId_copy(&other.id_, &id_); return *this; }
+    /// Create NodeId with String identifier from String wrapper class
+    NodeId(String identifier, UA_UInt16 namespaceIndex = 0) {
+        data_ = UA_NODEID_STRING_ALLOC(namespaceIndex, identifier.get().data());
+    }
 
-    // NodeId(NodeId&& other) = default;
-    // NodeId& operator=(NodeId&& other) = default;
+    /// Create NodeId with Guid identifier
+    NodeId(Guid identifier, UA_UInt16 namespaceIndex = 0) {
+        data_ = UA_NODEID_GUID(namespaceIndex, *identifier.handle());
+    }
 
-    // GUID and BYTESTRING not implemented
+    /// Create NodeId with ByteString identifier
+    NodeId(ByteString identifier, UA_UInt16 namespaceIndex = 0) {
+        data_ = UA_NODEID_BYTESTRING_ALLOC(namespaceIndex, identifier.get().data());
+    }
 
-    inline bool operator==(const NodeId& other) const { return UA_NodeId_equal(&id_, other.handle()); }
-    inline bool operator!=(const NodeId& other) const { return !operator==(other); }
+    inline bool operator==(const NodeId& other) const { return UA_NodeId_order(&data_, other.handle()) == UA_ORDER_EQ; }
+    inline bool operator!=(const NodeId& other) const { return UA_NodeId_order(&data_, other.handle()) != UA_ORDER_EQ; }
+    inline bool operator< (const NodeId& other) const { return UA_NodeId_order(&data_, other.handle()) == UA_ORDER_LESS; }
+    inline bool operator> (const NodeId& other) const { return UA_NodeId_order(&data_, other.handle()) == UA_ORDER_MORE; }
 
-    inline uint32_t hash() const { return UA_NodeId_hash(&id_); }
+    inline UA_UInt32 hash() const { return UA_NodeId_hash(&data_); }
 
-    inline auto getNamespaceIndex() const { return id_.namespaceIndex; }
+    inline UA_UInt16     getNamespaceIndex() const { return data_.namespaceIndex; }
+    inline UA_NodeIdType getIdentifierType() const { return data_.identifierType; }
 
-    inline       UA_NodeId* handle()       { return &id_; }
-    inline const UA_NodeId* handle() const { return &id_; }
-private:
-    UA_NodeId id_;
+    std::variant<UA_UInt32, String, Guid, ByteString> getIdentifier() const {
+        switch (data_.identifierType) {
+        case UA_NODEIDTYPE_NUMERIC:
+            return data_.identifier.numeric;
+        case UA_NODEIDTYPE_STRING:
+            return String(data_.identifier.string);
+        case UA_NODEIDTYPE_GUID:
+            return Guid(data_.identifier.guid);
+        case UA_NODEIDTYPE_BYTESTRING:
+            return ByteString(data_.identifier.byteString);
+        default:
+            return {};
+        }
+    }
 };
 
 } // namespace opc ua
