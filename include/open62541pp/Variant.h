8e6ea5c80d57a18710d20df16568e37992839bde
Lukas Berbuer <lukas.berbuer@gmail.com>
2019-10-01 23:20:16 +0200

    Rename setScalar/setArray -> writeScalar/writeArray and add comments

--- include/open62541pp/Variant.h
+++ include/open62541pp/Variant.h
@@ -15,19 +15,29 @@ class Variant : public TypeWrapper<UA_Variant> {
 public:
     using BaseClass::BaseClass; // inherit contructors
 
+    /// Constructor for scalars
     template <typename T>
-    Variant(const T& value) { writeScalar<T>(value); }
-    template <typename T>
-    Variant(const std::vector<T>& vector) { writeArray<T>(vector); }
+    Variant(const T& value) { setScalar<T>(value); }
 
+    /// Constructor for arrays
+    template <typename T>
+    Variant(const std::vector<T>& vector) { setArray<T>(vector); }
+
+    /// Check if variant is empty
     inline bool isEmpty()  const noexcept { return UA_Variant_isEmpty(&data_); }
+    /// Check if variant is a scalar
     inline bool isScalar() const noexcept { return UA_Variant_isScalar(&data_); }
+    /// Check if variant is a array
     inline bool isArray()  const noexcept { return (data_.arrayLength > 0 && data_.data != UA_EMPTY_ARRAY_SENTINEL); }
-    
+
+    /// Check if variant type is equal to template type
     template <typename T>
     inline bool isType()          const noexcept { return data_.type == getUaDataType<T>(); }
+    /// Check if variant type is equal to type argument (enum)
     inline bool isType(Type type) const noexcept { return data_.type == getUaDataType(type); }
 
+    /// Read scalar value with given template type.
+    /// An exception is thrown if the variant is not a scalar or not of the given type.
     template <typename T>
     T readScalar() const {
         if (!isScalar())
@@ -37,6 +47,8 @@ public:
         return *static_cast<T*>(data_.data); // copy on purpose
     }
 
+    /// Read array with given template type and return is as a std::vector-
+    /// An exception is thrown if the variant is not an array or not of the given type.
     template <typename T>
     std::vector<T> readArray() const {
         if (!isArray())
@@ -53,16 +65,18 @@ public:
         return result;
     }
 
+    /// Write scalar value to variant.
     template <typename T>
-    void writeScalar(T value) {
+    void setScalar(T value) {
         clear();
         auto status = UA_Variant_setScalarCopy(&data_, &value, getUaDataType<T>());
         checkStatusCodeException(status);
         data_.storageType = UA_VARIANT_DATA;
     }
 
+    /// Write array (std::vector) to variant.
     template <typename T>
-    void writeArray(const std::vector<T>& array) {
+    void setArray(const std::vector<T>& array) {
         clear();
         auto status = UA_Variant_setArrayCopy(&data_,
             array.data(),
@@ -72,8 +86,11 @@ public:
         data_.storageType = UA_VARIANT_DATA;
     }
 
+    /// Write array (std::vector) to variant by passing it's address to the variant (no copy).
+    /// If the array values change, the variant values change as well.
+    /// Take care of the vectors lifttime. If it gets deconstructed while the variant is still used, data get's corrupted.
     template <typename T>
-    void writeArrayNoCopy(std::vector<T>& array) noexcept {
+    void setArrayNoCopy(std::vector<T>& array) noexcept {
         clear();
         // UA_Variant_setArray will borrow the vector data compared to UA_Variant_setArrayCopy
         UA_Variant_setArray(&data_,
