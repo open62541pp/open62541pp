86a750d2ad61e5c5ba3f75dc5b73a7219c26c094
Eduardo M. Lopes <505902+duca@users.noreply.github.com>
2023-03-09 00:52:02 -0300

    working on a NodeClient class
    Got the original Node and adapted to the client side of the link.
    TODO add the NodeClient tests and the subscription

---
+++ include/open62541pp/NodeClient.h
@@ -0,0 +1,254 @@
+#pragma once
+
+#include "open62541pp/ErrorHandling.h"
+#include "open62541pp/NodeId.h"
+#include "open62541pp/Client.h"
+#include "open62541pp/TypeWrapper.h"
+#include "open62541pp/Types.h"
+#include "open62541pp/Variant.h"
+
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <memory>
+
+namespace opcua {
+
+/**
+ * High-level node object, to access node attribute, browse and populate address space.
+ *
+ * Node objects are usefull as-is but they do not expose the entire OPC UA protocol.
+ * You can get access to the associated NodeId instance with the getNodeId() method and apply the
+ * native open62541 functions.
+ */
+class NodeClient {
+public:
+    NodeClient(std::shared_ptr<Client> client_, const NodeId& id_);
+
+    /// Get server instance.
+    std::shared_ptr<Client> getClient() noexcept;
+
+    /// Get server instance.
+    std::shared_ptr<Client> getClient() const noexcept;
+
+    /// Get node id.
+    const NodeId& getNodeId() const noexcept;
+
+    /// Get node class.
+    NodeClass getNodeClass() const noexcept;
+
+    /// Get browse name.
+    std::string getBrowseName();
+    /// Get localized display name.
+    LocalizedText getDisplayName();
+    /// Get localized description.
+    LocalizedText getDescription();
+    /// Get write mask, e.g. `::UA_WRITEMASK_ACCESSLEVEL | ::UA_WRITEMASK_DESCRIPTION`.
+    uint32_t getWriteMask();
+    // uint32_t getUserWriteMask();
+
+    /// Get data type of variable (type) node as NodeId.
+    NodeId getDataType();
+    /// Get value rank of variable (type) node.
+    ValueRank getValueRank();
+    /// Get array dimensions of variable (type) node.
+    // std::vector<uint32_t> getArrayDimensions();
+    /// Get access level mask of variable node, e.g. `::UA_ACCESSLEVELMASK_READ`.
+    uint8_t getAccessLevel();
+
+    // writeBrowseName disabled for performance reasons:
+    // https://github.com/open62541/open62541/issues/3545
+    // void setBrowseName(std::string_view name);
+
+    /// Set localized display name.
+    void setDisplayName(std::string_view name, std::string_view locale);
+    /// Set localized description.
+    void setDescription(std::string_view name, std::string_view locale);
+    /// Set write mask, e.g. `::UA_WRITEMASK_ACCESSLEVEL | ::UA_WRITEMASK_DESCRIPTION`.
+    void setWriteMask(uint32_t mask);
+    // void setUserWriteMask(uint32_t mask);
+
+    /// Set data type of variable (type) node.
+    void setDataType(Type type);
+    /// Set data type of variable (type) node by node id.
+    void setDataType(const NodeId& typeId);
+    /// Set value rank of variable (type) node.
+    void setValueRank(ValueRank valueRank);
+    /// Set array dimensions of variable (type) node.
+    /// Should be unspecified if ValueRank is <= 0 (ValueRank::Any, ValueRank::Scalar,
+    /// ValueRank::ScalarOrOneDimension, ValueRank::OneOrMoreDimensions). The dimension zero is a
+    /// wildcard and the actual value may have any length in this dimension.
+    void setArrayDimensions(const std::vector<uint32_t>& dimensions);
+    /// Set access level mask of variable node,
+    /// e.g. `::UA_ACCESSLEVELMASK_READ | ::UA_ACCESSLEVELMASK_WRITE`.
+    void setAccessLevel(uint8_t mask);
+    // /// Set modelling rule.
+    // void setModellingRule(ModellingRule rule);
+
+    /// Add child folder to node.
+    // NodeClient addFolder(
+    //     const NodeId& id,
+    //     std::string_view browseName,
+    //     ReferenceType referenceType = ReferenceType::HasComponent
+    // );
+
+    /// Add child object to node.
+    // NodeClient addObject(
+    //     const NodeId& id,
+    //     std::string_view browseName,
+    //     const NodeId& objectType = {UA_NS0ID_BASEOBJECTTYPE, 0},
+    //     ReferenceType referenceType = ReferenceType::HasComponent
+    // );
+
+    /// Add child variable to node.
+    // NodeClient addVariable(
+    //     const NodeId& id,
+    //     std::string_view browseName,
+    //     const NodeId& variableType = {UA_NS0ID_BASEDATAVARIABLETYPE, 0},
+    //     ReferenceType referenceType = ReferenceType::HasComponent
+    // );
+
+    /// Add child property to node.
+    // NodeClient addProperty(const NodeId& id, std::string_view browseName);
+
+    /// Add child object type to node.
+    // NodeClient addObjectType(
+    //     const NodeId& id,
+    //     std::string_view browseName,
+    //     ReferenceType referenceType = ReferenceType::HasSubType
+    // );
+
+    /// Add child variable type to node.
+    // NodeClient addVariableType(
+    //     const NodeId& id,
+    //     std::string_view browseName,
+    //     const NodeId& variableType = {UA_NS0ID_BASEDATAVARIABLETYPE, 0},
+    //     ReferenceType referenceType = ReferenceType::HasSubType
+    // );
+
+    /// Add reference.
+    void addReference(const NodeId& target, ReferenceType referenceType, bool forward = true);
+
+    /// Get a child specified by its path from this node (only local nodes).
+    /// @exception BadStatus If path not found (BadNoMatch)
+    // NodeClient getChild(const std::vector<QualifiedName>& path);
+
+    // Read value from variable node as DataValue object.
+    // void readDataValue(DataValue& dataValue);
+
+    /// Read value from variable node as Variant object.
+    void readValue(Variant& variant);
+
+    /// Read scalar from variable node.
+    template <typename T>
+    T readScalar();
+
+    /// Read array from variable node.
+    template <typename T>
+    std::vector<T> readArray();
+
+    // Write DataValue to variable node.
+    // void writeDataValue(const DataValue& variant);
+
+    /// Write Variant to variable node.
+    void writeValue(const Variant& variant);
+
+    /// Write scalar to variable node.
+    template <typename T, Type type = detail::guessType<T>()>
+    void writeScalar(const T& value);
+
+    /// Write array (raw) to variable node.
+    template <typename T, Type type = detail::guessType<T>()>
+    void writeArray(const T* array, size_t size);
+
+    /// Write array (std::vector) to variable node.
+    template <typename T, Type type = detail::guessType<T>()>
+    void writeArray(const std::vector<T>& array);
+
+    /// Write range of elements as array to variable node.
+    template <typename InputIt, Type type = detail::guessTypeFromIterator<InputIt>()>
+    void writeArray(InputIt first, InputIt last);
+
+    void remove(bool deleteReferences = true);
+
+protected:
+    template <typename... Ts>
+    constexpr bool isNodeClass(Ts&&... classes) {
+        const auto isSame = [&](NodeClass c) { return nodeClass_ == c; };
+        return (isSame(classes) || ...);
+    }
+
+    template <typename... Ts>
+    void requireNodeClass(Ts&&... classes) {
+        const bool isAnyOf = isNodeClass(std::forward<Ts>(classes)...);
+        if (!isAnyOf) {
+            const auto nodeClassName = getNodeClassName(nodeClass_);
+            throw InvalidNodeClass(
+                std::string("Operation not allowed for nodes of class ").append(nodeClassName)
+            );
+        }
+    }
+
+private:
+    std::shared_ptr<Client> client_;
+    NodeId nodeId_;
+    NodeClass nodeClass_;
+};
+
+/* ---------------------------------------------------------------------------------------------- */
+
+template <typename T>
+T NodeClient::readScalar() {
+    Variant variant;
+    readValue(variant);
+    return variant.getScalarCopy<T>();
+}
+
+template <typename T>
+std::vector<T> NodeClient::readArray() {
+    Variant variant;
+    readValue(variant);
+    return variant.getArrayCopy<T>();
+}
+
+template <typename T, Type type>
+void NodeClient::writeScalar(const T& value) {
+    Variant variant;
+    if constexpr (detail::isAssignableToVariantScalar<T>()) {
+        variant.setScalar<T, type>(const_cast<T&>(value));  // NOLINT, variant isn't modified
+    } else {
+        variant.setScalarCopy<T, type>(value);
+    }
+    writeValue(variant);
+}
+
+template <typename T, Type type>
+void NodeClient::writeArray(const T* array, size_t size) {
+    Variant variant;
+    if constexpr (detail::isAssignableToVariantArray<T>()) {
+        variant.setArray<T, type>(const_cast<T*>(array), size);  // NOLINT, variant isn't modified
+    } else {
+        variant.setArrayCopy<T, type>(array, size);
+    }
+    writeValue(variant);
+}
+
+template <typename T, Type type>
+void NodeClient::writeArray(const std::vector<T>& array) {
+    writeArray<T, type>(array.data(), array.size());
+}
+
+template <typename InputIt, Type type>
+void NodeClient::writeArray(InputIt first, InputIt last) {
+    Variant variant;
+    variant.setArrayCopy<InputIt, type>(first, last);
+    writeValue(variant);
+}
+
+/* ---------------------------------------------------------------------------------------------- */
+
+bool operator==(const NodeClient& left, const NodeClient& right) noexcept;
+bool operator!=(const NodeClient& left, const NodeClient& right) noexcept;
+
+}  // namespace opcua
---
+++ src/NodeClient.cpp
@@ -0,0 +1,344 @@
+#include "open62541pp/NodeClient.h"
+
+#include "open62541pp/ErrorHandling.h"
+#include "open62541pp/Helper.h"
+#include "open62541pp/TypeWrapper.h"
+#include "open62541pp/TypeConverter.h"
+using namespace opcua::detail;
+
+#include "open62541/client_highlevel.h"
+#include "open62541_impl.h"
+
+namespace opcua {
+NodeClient::NodeClient(std::shared_ptr<Client> client, const NodeId& id)  // NOLINT
+    : client_(client),
+      nodeId_(id) {
+    // check if node exists
+    {
+        NodeId outputNode(UA_NODEID_NULL);
+        const auto status = UA_Client_readNodeIdAttribute(client_->handle(), nodeId_, outputNode.handle());
+        detail::throwOnBadStatus(status);
+    }
+    // store node class
+    {
+        UA_NodeClass nodeClass = UA_NODECLASS_UNSPECIFIED;
+        const auto status = UA_Client_readNodeClassAttribute(client_->handle(), nodeId_, &nodeClass);
+        detail::throwOnBadStatus(status);
+        nodeClass_ = static_cast<NodeClass>(nodeClass);
+    }
+}
+
+std::shared_ptr<Client> NodeClient::getClient() noexcept {
+    return client_;
+}
+
+std::shared_ptr<Client> NodeClient::getClient() const noexcept {
+    return client_;
+}
+
+const NodeId& NodeClient::getNodeId() const noexcept {
+    return nodeId_;
+}
+
+NodeClass NodeClient::getNodeClass() const noexcept {
+    return nodeClass_;
+}
+
+std::string NodeClient::getBrowseName() {
+    QualifiedName name;
+    const auto status = UA_Client_readBrowseNameAttribute(client_->handle(), nodeId_, name.handle());
+    detail::throwOnBadStatus(status);
+    return name.getName();
+}
+
+LocalizedText NodeClient::getDisplayName() {
+    LocalizedText text;
+    const auto status = UA_Client_readDisplayNameAttribute(client_->handle(), nodeId_, text.handle());
+    detail::throwOnBadStatus(status);
+    return text;
+}
+
+LocalizedText NodeClient::getDescription() {
+    LocalizedText text;
+    const auto status = UA_Client_readDescriptionAttribute(client_->handle(), nodeId_, text.handle());
+    detail::throwOnBadStatus(status);
+    return text;
+}
+
+uint32_t NodeClient::getWriteMask() {
+    uint32_t writeMask = 0;
+    const auto status = UA_Client_readWriteMaskAttribute(client_->handle(), nodeId_, &writeMask);
+    detail::throwOnBadStatus(status);
+    return writeMask;
+}
+
+NodeId NodeClient::getDataType() {
+    NodeId nodeId(0, 0);
+    const auto status = UA_Client_readDataTypeAttribute(client_->handle(), nodeId_, nodeId.handle());
+    detail::throwOnBadStatus(status);
+    return nodeId;
+}
+
+ValueRank NodeClient::getValueRank() {
+    int32_t valueRank = 0;
+    const auto status = UA_Client_readValueRankAttribute(client_->handle(), nodeId_, &valueRank);
+    detail::throwOnBadStatus(status);
+    return static_cast<ValueRank>(valueRank);
+}
+
+// std::vector<uint32_t> NodeClient::getArrayDimensions() {
+//     Variant variant;
+//     const auto status = UA_Client_readArrayDimensionsAttribute(client_->handle(), nodeId_, variant.handle());
+//     detail::throwOnBadStatus(status);
+//     if (variant.isArray()) {
+//         return variant.getArrayCopy<uint32_t>();
+//     }
+//     return {};
+// }
+
+uint8_t NodeClient::getAccessLevel() {
+    uint8_t mask = 0;
+    const auto status = UA_Client_readAccessLevelAttribute(client_->handle(), nodeId_, &mask);
+    detail::throwOnBadStatus(status);
+    return mask;
+}
+
+void NodeClient::setDisplayName(std::string_view name, std::string_view locale) {
+    const auto status = UA_Client_writeDisplayNameAttribute(
+        client_->handle(), nodeId_, LocalizedText(name, locale).handle()
+    );
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::setDescription(std::string_view name, std::string_view locale) {
+    const auto status = UA_Client_writeDescriptionAttribute(
+        client_->handle(), nodeId_, LocalizedText(name, locale).handle()
+    );
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::setWriteMask(uint32_t mask) {
+    const auto status = UA_Client_writeWriteMaskAttribute(client_->handle(), nodeId_, &mask);
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::setDataType(Type type) {
+    const auto status = UA_Client_writeDataTypeAttribute(
+        client_->handle(), nodeId_, &detail::getUaDataType(type)->typeId
+    );
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::setDataType(const NodeId& typeId) {
+    const auto status = UA_Client_writeDataTypeAttribute(client_->handle(), *nodeId_.handle(), typeId.handle());
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::setValueRank(ValueRank valueRank) {
+    int rank = static_cast<int32_t>(valueRank);
+    const auto status = UA_Client_writeValueRankAttribute(
+        client_->handle(), nodeId_, &rank
+    );
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::setArrayDimensions(const std::vector<uint32_t>& dimensions) {
+    Variant variant;
+    variant.setArrayCopy(dimensions);
+    auto const arr = variant.getArray<UA_UInt32>();
+    const auto status = UA_Client_writeArrayDimensionsAttribute(client_->handle(), nodeId_, variant.getArrayLength(), arr);
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::setAccessLevel(uint8_t mask) {
+    UA_Byte m = static_cast<UA_Byte>(mask);
+    const auto status = UA_Client_writeAccessLevelAttribute(
+        client_->handle(), nodeId_, &m
+    );
+    detail::throwOnBadStatus(status);
+}
+
+// void NodeClient::setModellingRule(ModellingRule rule) {
+//     const auto status = UA_Client_addReference(
+//         client_->handle(),  // client
+//         nodeId_,  // source id
+//         UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),  // reference id
+//         true,  // forward
+//         UA_EXPANDEDNODEID_NUMERIC(0, static_cast<UA_UInt32>(rule)),  // target id
+//         true  // forward
+//     );
+//     detail::throwOnBadStatus(status);
+// }
+
+// NodeClient NodeClient::addFolder(const NodeId& id, std::string_view browseName, ReferenceType referenceType) {
+//     const auto status = UA_Client_addObjectNode(
+//         client_->handle(),  // client
+//         id,  // new requested id
+//         nodeId_,  // parent id
+//         detail::getUaNodeId(referenceType),  // reference id
+//         QualifiedName(id.getNamespaceIndex(), browseName),  // browse name
+//         UA_NODEID_NUMERIC(0, UA_NS0ID_FOLDERTYPE),  // type definition
+//         UA_ObjectAttributes_default,  // object attributes
+//         nullptr,  // node context
+//         nullptr  // output new node id
+//     );
+//     detail::throwOnBadStatus(status);
+//     return {client_, id};
+// }
+
+// NodeClient NodeClient::addObject(
+//     const NodeId& id,
+//     std::string_view browseName,
+//     const NodeId& objectType,
+//     ReferenceType referenceType
+// ) {
+//     const auto status = UA_Client_addObjectNode(
+//         client_->handle(),  // client
+//         id,  // new requested id
+//         nodeId_,  // parent id
+//         detail::getUaNodeId(referenceType),  // reference id
+//         QualifiedName(id.getNamespaceIndex(), browseName),  // browse name
+//         objectType,  // type definition
+//         UA_ObjectAttributes_default,  // object attributes
+//         nullptr,  // node context
+//         nullptr  // output new node id
+//     );
+//     detail::throwOnBadStatus(status);
+//     return {client_, id};
+// }
+
+// NodeClient NodeClient::addVariable(
+//     const NodeId& id,
+//     std::string_view browseName,
+//     const NodeId& variableType,
+//     ReferenceType referenceType
+// ) {
+//     const auto status = UA_Client_addVariableNode(
+//         client_->handle(),  // client
+//         id,  // new requested id
+//         nodeId_,  // parent id
+//         detail::getUaNodeId(referenceType),  // reference id
+//         QualifiedName(id.getNamespaceIndex(), browseName),  // browse name
+//         variableType,  // type definition
+//         UA_VariableAttributes_default,  // variable attributes
+//         nullptr,  // node context
+//         nullptr  // output new node id
+//     );
+//     detail::throwOnBadStatus(status);
+//     return {client_, id};
+// }
+
+// NodeClient NodeClient::addProperty(const NodeId& id, std::string_view browseName) {
+//     const auto status = UA_Client_addVariableNode(
+//         client_->handle(),  // client
+//         id,  // new requested id
+//         nodeId_,  // parent id
+//         UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY),  // reference id
+//         QualifiedName(id.getNamespaceIndex(), browseName),  // browse name
+//         UA_NODEID_NUMERIC(0, UA_NS0ID_PROPERTYTYPE),  // type definition
+//         UA_VariableAttributes_default,  // variable attributes
+//         nullptr,  // node context
+//         nullptr  // output new node id
+//     );
+//     detail::throwOnBadStatus(status);
+//     return {client_, id};
+// }
+
+// NodeClient NodeClient::addObjectType(
+//     const NodeId& id, std::string_view browseName, ReferenceType referenceType
+// ) {
+//     const auto status = UA_Client_addObjectTypeNode(
+//         client_->handle(),  // client
+//         id,  // new requested id
+//         nodeId_,  // parent id
+//         detail::getUaNodeId(referenceType),  // reference id
+//         QualifiedName(id.getNamespaceIndex(), browseName),  // browse name
+//         UA_ObjectTypeAttributes_default,  // object attributes
+//         nullptr,  // node context
+//         nullptr  // output new node id
+//     );
+//     detail::throwOnBadStatus(status);
+//     return {client_, id};
+// }
+
+// NodeClient NodeClient::addVariableType(
+//     const NodeId& id,
+//     std::string_view browseName,
+//     const NodeId& variableType,
+//     ReferenceType referenceType
+// ) {
+//     const auto status = UA_Client_addVariableTypeNode(
+//         client_->handle(),  // client
+//         id,  // new requested id
+//         nodeId_,  // parent id
+//         detail::getUaNodeId(referenceType),  // reference id
+//         QualifiedName(id.getNamespaceIndex(), browseName),  // browse name
+//         variableType,  // type definition
+//         UA_VariableTypeAttributes_default,  // variable attributes
+//         nullptr,  // node context
+//         nullptr  // output new node id
+//     );
+//     detail::throwOnBadStatus(status);
+//     return {client_, id};
+// }
+
+// void NodeClient::addReference(const NodeId& target, ReferenceType referenceType, bool forward) {
+//     const auto status = UA_Client_addReference(
+//         client_->handle(),  // client
+//         nodeId_,  // source id
+//         detail::getUaNodeId(referenceType),  // reference id
+//         ExpandedNodeId(target, {}, 0),  // target id
+//         forward  // forward
+//     );
+//     detail::throwOnBadStatus(status);
+// }
+
+// NodeClient NodeClient::getChild(const std::vector<QualifiedName>& path) {
+//     const std::vector<UA_QualifiedName> pathNative(path.begin(), path.end());
+//     const TypeWrapper<UA_BrowsePathResult, UA_TYPES_BROWSEPATHRESULT> result(
+//         UA_Client_browseSimplifiedBrowsePath(
+//             client_->handle(),
+//             nodeId_,  // origin
+//             pathNative.size(),  // browse path size
+//             pathNative.data()  // browse path
+//         )
+//     );
+//     detail::throwOnBadStatus(result->statusCode);
+//     assert(result->targets != nullptr && result->targetsSize >= 1);  // NOLINT
+//     const auto id = ExpandedNodeId(result->targets[0].targetId);  // NOLINT
+//     if (!id.isLocal()) {
+//         throw BadStatus(UA_STATUSCODE_BADNOMATCH);
+//     }
+//     return {client_, id.getNodeId()};
+// }
+
+void NodeClient::writeValue(const Variant& var) {
+    const auto status = UA_Client_writeValueAttribute(client_->handle(), nodeId_, var.handle());
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::readValue(Variant& var) {
+    const auto status = UA_Client_readValueAttribute(client_->handle(), nodeId_, var.handle());
+    detail::throwOnBadStatus(status);
+}
+
+void NodeClient::remove(bool deleteReferences) {
+    const auto status = UA_Client_deleteNode(client_->handle(), *nodeId_.handle(), deleteReferences);
+    detail::throwOnBadStatus(status);
+}
+
+/* ---------------------------------------------------------------------------------------------- */
+
+bool operator==(const NodeClient& left, const NodeClient& right) noexcept {
+    auto const l = left.getClient().get();
+    auto const r = right.getClient().get();
+
+    return (l == r) &&
+    (left.getNodeId() == right.getNodeId());
+}
+
+bool operator!=(const NodeClient& left, const NodeClient& right) noexcept {
+    return !(left == right);
+}
+
+}  // namespace opcua
