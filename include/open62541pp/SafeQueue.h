d8f63d16b6a0321a9ad64b6ece5563c36d018c52
Eduardo Lopes <505902+duca@users.noreply.github.com>
2023-03-12 23:53:11 -0300

    Thread safe queue implementation added

---
+++ include/open62541pp/SafeQueue.h
@@ -0,0 +1,45 @@
+//
+// Created by eduardo on 12/03/23.
+//
+
+#pragma once
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+namespace opcua {
+
+/**
+ * Thread safe queue taken from https://codereview.stackexchange.com/questions/267847/thread-safe-message-queue
+ * @tparam T
+ */
+template <typename T>
+class SafeQueue {
+    std::mutex mutex;
+    std::condition_variable cond_var;
+    std::queue<T> queue;
+
+public:
+    void push(T&& item) {
+        {
+            std::lock_guard lock(mutex);
+            queue.push(item);
+        }
+
+        cond_var.notify_one();
+    }
+
+    T& front() {
+        std::unique_lock lock(mutex);
+        cond_var.wait(lock, [&]{ return !queue.empty(); });
+        return queue.front();
+    }
+
+    void pop() {
+        std::lock_guard lock(mutex);
+        queue.pop(item);
+    }
+};
+
+}  // namespace opcua
