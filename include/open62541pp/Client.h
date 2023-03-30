491a47030622bd4bc2fee08bb2d72ff7b505c9c8
Eduardo M. Lopes <505902+duca@users.noreply.github.com>
2023-03-30 17:08:31 -0300

    added a connected check

--- include/open62541pp/Client.h
+++ include/open62541pp/Client.h
@@ -13,13 +13,13 @@
 #include <open62541/client.h>
 
 #include <memory>
-#include <string_view>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace opcua {
 
-    // forward declaration
+// forward declaration
 class NodeId;
 class Node;
 
@@ -28,82 +28,86 @@ class Node;
 class Client {
 
 public:
-
-	Client();
+    Client();
 
     ~Client();
 
-	// // delete unused constructors
-	Client(Client &other) = delete;
-	Client(Client const& other) = delete;
-	Client(Client &&other) = delete;
+    // // delete unused constructors
+    Client(Client& other) = delete;
+    Client(const Client& other) = delete;
+    Client(Client&& other) = delete;
 
-	/**
-	   @brief find servers and return a vector of server names and their urls
-	   @param[in] uri of network to search for servers
-	   @return vector of pairs of servername and urls
-	   @warning throws opcua::BadStatus
-	*/
-    [[nodiscard]] std::vector<std::pair<std::string, std::string>> findServers(std::string_view url);
+    /**
+       @brief find servers and return a vector of server names and their urls
+       @param[in] uri of network to search for servers
+       @return vector of pairs of servername and urls
+       @warning throws opcua::BadStatus
+    */
+    [[nodiscard]] std::vector<std::pair<std::string, std::string>> findServers(std::string_view url
+    );
 
-
-	/**
-	   @brief get a vector of endpoints
-	   @pparam[in] url of the server
-	   @return vector of Endpoints
-	   @warning throws opcua::BadStatus
-	*/
+    /**
+       @brief get a vector of endpoints
+       @pparam[in] url of the server
+       @return vector of Endpoints
+       @warning throws opcua::BadStatus
+    */
     [[nodiscard]] std::vector<opcua::Endpoint> getEndpoints(std::string_view url);
 
-	/**
-	   @brief Connect using a single url
-	   @param[in] url of the server
-	   @warning Throws opcua::BadStatus
-	 */
-	void connect(std::string_view url);
+    /**
+       @brief Connect using a single url
+       @param[in] url of the server
+       @warning Throws opcua::BadStatus
+     */
+    void connect(std::string_view url);
 
-	/**
-	   @brief Connect using username and a password
-	   @param[in] url of the server
-	   @param[in] username name of the user
-	   @param[in] user password
-	   @warning Throws opcua::BadStatus
-	 */
-	void connect(std::string_view url, std::string_view username, std::string_view password);
+    /**
+       @brief Connect using username and a password
+       @param[in] url of the server
+       @param[in] username name of the user
+       @param[in] user password
+       @warning Throws opcua::BadStatus
+     */
+    void connect(std::string_view url, std::string_view username, std::string_view password);
 
+    /**
+     * @brief return if the client is connected
+     * @return connected
+     */
+    bool connected();
 
-	/**
-	 * @brief Read Value Attribute from a node
-	 *
-	 * @param nodeId[in] id of the node
-	 * @return attribute value in a varian form
-	 */
-	Variant	 readValueAttribute(NodeId const& nodeId);
+    /**
+     * @brief Read Value Attribute from a node
+     *
+     * @param nodeId[in] id of the node
+     * @return attribute value in a varian form
+     */
+    Variant readValueAttribute(const NodeId& nodeId);
 
-	/**
-	 * @brief Write a value attribute to a node
-	 *
-	 * @param nodeId[in] id of the node
-	 * @param value[in] to be written
-	 */
-	void writeValueAttribute(NodeId const& nodeId, Variant const& value);
+    /**
+     * @brief Write a value attribute to a node
+     *
+     * @param nodeId[in] id of the node
+     * @param value[in] to be written
+     */
+    void writeValueAttribute(const NodeId& nodeId, const Variant& value);
 
-	/**
-	 * @brief accessor to the internal UA_Client
-	 *
-	 * @return UA_Client*
-	 */
-	UA_Client* handle() noexcept;
+    /**
+     * @brief accessor to the internal UA_Client
+     *
+     * @return UA_Client*
+     */
+    UA_Client* handle() noexcept;
 
-	/**
-	 * @brief const accessor to the internal UA_Client
-	 *
-	 * @return const UA_Client*
-	 */
-	const UA_Client* handle() const noexcept;
+    /**
+     * @brief const accessor to the internal UA_Client
+     *
+     * @return const UA_Client*
+     */
+    const UA_Client* handle() const noexcept;
 
 private:
-	struct PrivateData;
-	std::unique_ptr<PrivateData> m_d;
+    struct PrivateData;
+    std::unique_ptr<PrivateData> m_d;
 };
-}
+}  // namespace opcua
--- src/Client.cpp
+++ src/Client.cpp
@@ -7,97 +7,100 @@
 
 using namespace opcua;
 
-
 struct Client::PrivateData {
-	UA_Client* client{nullptr};
+    UA_Client* client{nullptr};
 };
 
-Client::Client() : m_d(new Client::PrivateData)
-{
-	m_d->client = UA_Client_new();
-	UA_ClientConfig_setDefault(UA_Client_getConfig(m_d->client));
+Client::Client()
+    : m_d(new Client::PrivateData) {
+    m_d->client = UA_Client_new();
+    UA_ClientConfig_setDefault(UA_Client_getConfig(m_d->client));
 }
 
-Client::~Client()
-{
-	if (m_d->client)
-		UA_Client_delete(m_d->client);
+Client::~Client() {
+    if (m_d->client)
+        UA_Client_delete(m_d->client);
 }
 
-std::vector<std::pair<std::string, std::string>> Client::findServers(std::string_view url)
-{
-	size_t nServers{0};
-	UA_ApplicationDescription *registeredServers = nullptr;
+std::vector<std::pair<std::string, std::string>> Client::findServers(std::string_view url) {
+    size_t nServers{0};
+    UA_ApplicationDescription* registeredServers = nullptr;
 
-	if(detail::isBadStatus(UA_Client_findServers(m_d->client, url.data(), 0, nullptr, 0, nullptr,
-												 &nServers, &registeredServers)))
-	   return {};
+    if (detail::isBadStatus(UA_Client_findServers(
+            m_d->client, url.data(), 0, nullptr, 0, nullptr, &nServers, &registeredServers
+        )))
+        return {};
 
-	std::vector<std::pair<std::string, std::string>> res;
+    std::vector<std::pair<std::string, std::string>> res;
 
-	for (size_t i=0; i< nServers; ++i)
-	{
-		res.emplace_back(std::pair<std::string, std::string>{
-				detail::toString(registeredServers[i].applicationName.text),
-				detail::toString(registeredServers[i].applicationUri)});
-	}
+    for (size_t i = 0; i < nServers; ++i) {
+        res.emplace_back(std::pair<std::string, std::string>{
+            detail::toString(registeredServers[i].applicationName.text),
+            detail::toString(registeredServers[i].applicationUri)});
+    }
 
-	UA_Array_delete(registeredServers, nServers, &UA_TYPES[UA_TYPES_APPLICATIONDESCRIPTION]);
-	return res;
+    UA_Array_delete(registeredServers, nServers, &UA_TYPES[UA_TYPES_APPLICATIONDESCRIPTION]);
+    return res;
 }
 
-std::vector<Endpoint> Client::getEndpoints(std::string_view url)
-{
-	UA_EndpointDescription *descArray = nullptr;
-	size_t descArraySize = 0;
+std::vector<Endpoint> Client::getEndpoints(std::string_view url) {
+    UA_EndpointDescription* descArray = nullptr;
+    size_t descArraySize = 0;
 
-	detail::throwOnBadStatus(UA_Client_getEndpoints(m_d->client, url.data(),
-													&descArraySize, &descArray));
+    detail::throwOnBadStatus(
+        UA_Client_getEndpoints(m_d->client, url.data(), &descArraySize, &descArray)
+    );
 
-	std::vector<Endpoint> ret;
+    std::vector<Endpoint> ret;
 
-	for (size_t i=0; i < descArraySize; ++i){
-		Endpoint point;
-		point.url = detail::toString(descArray[i].endpointUrl);
-		TypeConverter<ApplicationDescription>::fromNative(descArray[i].server, point.server);
-		point.serverCertificate = detail::toString(descArray[i].serverCertificate);
+    for (size_t i = 0; i < descArraySize; ++i) {
+        Endpoint point;
+        point.url = detail::toString(descArray[i].endpointUrl);
+        TypeConverter<ApplicationDescription>::fromNative(descArray[i].server, point.server);
+        point.serverCertificate = detail::toString(descArray[i].serverCertificate);
 
-		ret.emplace_back(std::move(point));
-	}
+        ret.emplace_back(std::move(point));
+    }
 
-	UA_Array_delete(descArray, descArraySize, &UA_TYPES[UA_TYPES_ENDPOINTDESCRIPTION]);
-	return ret;
+    UA_Array_delete(descArray, descArraySize, &UA_TYPES[UA_TYPES_ENDPOINTDESCRIPTION]);
+    return ret;
 }
 
-void Client::connect(std::string_view url)
-{
-	detail::throwOnBadStatus(UA_Client_connect(m_d->client, url.data()));
+void Client::connect(std::string_view url) {
+    detail::throwOnBadStatus(UA_Client_connect(m_d->client, url.data()));
 }
 
-void Client::connect(std::string_view url, std::string_view username, std::string_view password)
-{
-	detail::throwOnBadStatus(UA_Client_connectUsername(m_d->client, url.data(), username.data(), password.data()));
+void Client::connect(std::string_view url, std::string_view username, std::string_view password) {
+    detail::throwOnBadStatus(
+        UA_Client_connectUsername(m_d->client, url.data(), username.data(), password.data())
+    );
 }
 
-Variant	 Client::readValueAttribute(NodeId const& nodeId)
-{
-	(void)nodeId;
-	return {};
+Variant Client::readValueAttribute(const NodeId& nodeId) {
+    (void)nodeId;
+    return {};
 }
 
-void Client::writeValueAttribute(NodeId const& nodeId, Variant const& value)
-{
-	(void)nodeId;
-	(void)value;
+void Client::writeValueAttribute(const NodeId& nodeId, const Variant& value) {
+    (void)nodeId;
+    (void)value;
 }
 
-UA_Client* Client::handle() noexcept
-{
-	return m_d->client;
+UA_Client* Client::handle() noexcept {
+    return m_d->client;
 }
 
-const UA_Client* Client::handle() const noexcept
-{
-	return m_d->client;
+const UA_Client* Client::handle() const noexcept {
+    return m_d->client;
 }
 
+bool Client::connected() {
+    UA_SecureChannelState* chanState = nullptr;
+    UA_SessionState* sessionState = nullptr;
+    UA_StatusCode* connectStatus = nullptr;
+
+    UA_Client_getState(m_d->client, chanState, sessionState, connectStatus);
+    detail::throwOnBadStatus(*connectStatus);
+
+    return *connectStatus == UA_SESSIONSTATE_ACTIVATED;
+}
