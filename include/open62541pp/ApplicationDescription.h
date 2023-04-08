6a4722c1636924e29b6125ff0640e17046f5eafa
Eduardo Lopes <elopes1@oceaneering.com>
2023-04-08 00:11:31 -0300

    fixed ApplicationDescription declaration and added proper setters and getters

--- include/open62541pp/ApplicationDescription.h
+++ include/open62541pp/ApplicationDescription.h
@@ -10,31 +10,127 @@
 
 namespace opcua {
 
-enum class ApplicationType { SERVER, CLIENT, CLIENT_AND_SERVER, DISCOVERY_SERVER, FORCE_32BITS };
+enum class ApplicationType {
+    SERVER = UA_APPLICATIONTYPE_SERVER,
+    CLIENT = UA_APPLICATIONTYPE_CLIENT,
+    CLIENT_AND_SERVER = UA_APPLICATIONTYPE_CLIENTANDSERVER,
+    DISCOVERY_SERVER = UA_APPLICATIONTYPE_DISCOVERYSERVER,
+    FORCE_32BITS = __UA_APPLICATIONTYPE_FORCE32BIT
+};
 
 class ApplicationDescription
     : TypeWrapper<UA_ApplicationDescription, UA_TYPES_APPLICATIONDESCRIPTION> {
 public:
     using TypeWrapperBase::TypeWrapperBase;  // inherit contructors
 
-    explicit ApplicationDescription(
-        std::string uri,
-        std::string productUri,
-        std::string name,
-        ApplicationType type,
-        std::string gatewayServerUri,
-        std::string discoveryProfileUri,
-        std::vector<std::string> discoveryUrls
-    );
+    /**
+     * @brief Get the application URI
+     * @return uri
+     */
+    const std::string& getUri() const;
 
-private:
-    std::string uri_;
-    std::string productUri_;
-    std::string name_;
-    ApplicationType type_;
-    std::string gatewayServerUri_;
-    std::string discoveryProfileUri_;
-    std::vector<std::string> discoveryUrls_;
+    /**
+     * @brief set the application URI
+     * @param uri
+     */
+    void setUri(const std::string& uri);
+
+    /**
+     * @brief Get the product URI
+     * @return product URI
+     */
+    const std::string& getProductUri() const;
+
+    /**
+     * @brief set the product URI
+     * @param productUri
+     */
+    void setProductUri(const std::string& productUri);
+
+    /**
+     * @brief Get the application name
+     * @return name
+     */
+    const LocalizedText& getName() const;
+
+    /**
+     * @brief set the application name
+     * @param name
+     */
+    void setName(const LocalizedText& name);
+
+    /**
+     * @brief Get the application type
+     * @return ApplicationType
+     */
+    ApplicationType getType() const;
+
+    /**
+     * @brief set the application type
+     * @param type
+     */
+    void setType(ApplicationType type);
+
+    /**
+     * @brief Get the gateway server URI
+     * @return gateway server URI
+     */
+    const std::string& getGatewayServerUri() const;
+
+    /**
+     * @brief set the gateway server URI
+     * @param gatewayServerUri
+     */
+    void setGatewayServerUri(const std::string& gatewayServerUri);
+
+    /**
+     * @brief Get the discovery profile URI
+     * @return discovery profile URI
+     */
+    const std::string& getDiscoveryProfileUri() const;
+
+    /**
+     * @brief set the discovery profile URI
+     * @param discoveryProfileUri
+     */
+    void setDiscoveryProfileUri(const std::string& discoveryProfileUri);
+
+    /**
+     * @brief Get the discovery Urls
+     * @return discovery Urls
+     */
+    const std::vector<std::string>& getDiscoveryUrls() const;
+
+    /**
+     * @brief set the discovery Urls
+     * @param discoveryUrls
+     */
+    void setDiscoveryUrls(const std::vector<std::string>& discoveryUrls);
+};
+
+template <>
+struct TypeConverter<ApplicationDescription> {
+    using ValueType = ApplicationDescription;
+    using NativeType = UA_ApplicationDescription;
+    using ValidTypes = TypeList<Type::ApplicationDescription>;
+
+    static void fromNative(const NativeType& src, ValueType& dst) {
+        dst = ApplicationDescription(src);
+    }
+
+    static void toNative(const ValueType& src, NativeType& dst) {
+        dst.applicationUri = detail::allocUaString(src.getUri());
+        dst.productUri = detail::allocUaString(src.getProductUri());
+        TypeConverter<std::string>::toNative(src.getName().getText(), dst.applicationName.text);
+        TypeConverter<std::string>::toNative(src.getName().getLocale(), dst.applicationName.locale);
+        dst.applicationType = static_cast<UA_ApplicationType>(src.getType());
+        dst.gatewayServerUri = detail::allocUaString(src.getGatewayServerUri());
+        dst.discoveryProfileUri = detail::allocUaString(src.getDiscoveryProfileUri());
+        dst.discoveryUrls =
+            detail::toNativeArrayAlloc<decltype(src.getDiscoveryUrls().begin()), Type::String>(
+                src.getDiscoveryUrls().begin(), src.getDiscoveryUrls().end()
+            );
+    }
 };
 
 }  // namespace opcua
--- src/ApplicationDescription.cpp
+++ src/ApplicationDescription.cpp
@@ -1,27 +1,73 @@
 #include "open62541pp/ApplicationDescription.h"
 
 #include "open62541pp/ErrorHandling.h"
+#include "open62541pp/TypeConverter.h"
 #include "open62541pp/Helper.h"
 
 #include "open62541_impl.h"
 
 namespace opcua {
 
-ApplicationDescription::ApplicationDescription (
-    std::string uri,
-    std::string productUri,
-    std::string name,
-    ApplicationType type,
-    std::string gatewayServerUri,
-    std::string discoveryProfileUri,
-    std::vector<std::string> discoveryUrls
-)
-    : uri_(uri),
-      productUri_(productUri),
-      name_(name),
-      type_(type),
-      gatewayServerUri_(gatewayServerUri),
-      discoveryProfileUri_(discoveryProfileUri),
-      discoveryUrls_(discoveryUrls) {}
+const std::string& ApplicationDescription::getUri() const {
+    return detail::toString(handle()->applicationUri);
+}
 
+void ApplicationDescription::setUri(const std::string& uri) {
+    TypeConverter<std::string>::toNative(uri, handle()->applicationUri);
+}
+
+const std::string& ApplicationDescription::getProductUri() const {
+    return detail::toString(handle()->productUri);
+}
+
+void ApplicationDescription::setProductUri(const std::string& productUri) {
+    TypeConverter<std::string>::toNative(productUri, handle()->productUri);
+}
+
+const LocalizedText& ApplicationDescription::getName() const {
+    LocalizedText ret;
+    TypeConverter<LocalizedText>::fromNative(handle()->applicationName, ret);
+    return ret;
+}
+
+void ApplicationDescription::setName(const LocalizedText& name) {
+    TypeConverter<std::string>::toNative(name.getText(), handle()->applicationName.text);
+    TypeConverter<std::string>::toNative(name.getLocale(), handle()->applicationName.locale);
+}
+
+ApplicationType ApplicationDescription::getType() const {
+    return static_cast<ApplicationType>(handle()->applicationType);
+}
+
+void ApplicationDescription::setType(ApplicationType type) {
+    handle()->applicationType = static_cast<UA_ApplicationType>(type);
+}
+
+const std::string& ApplicationDescription::getGatewayServerUri() const {
+    return detail::toString(handle()->gatewayServerUri);
+}
+
+void ApplicationDescription::setGatewayServerUri(const std::string& gatewayServerUri) {
+    TypeConverter<std::string>::toNative(gatewayServerUri, handle()->gatewayServerUri);
+}
+
+const std::string& ApplicationDescription::getDiscoveryProfileUri() const {
+    return detail::toString(handle()->discoveryProfileUri);
+}
+
+void ApplicationDescription::setDiscoveryProfileUri(const std::string& discoveryProfileUri) {
+    TypeConverter<std::string>::toNative(discoveryProfileUri, handle()->discoveryProfileUri);
+}
+
+const std::vector<std::string>& ApplicationDescription::getDiscoveryUrls() const {
+    return detail::fromNativeArray<std::string>(handle()->discoveryUrls, handle()->discoveryUrlsSize);
+}
+
+void ApplicationDescription::setDiscoveryUrls(const std::vector<std::string>& discoveryUrls) {
+    handle()->discoveryUrlsSize = discoveryUrls.size();
+    handle()->discoveryUrls = detail::allocNativeArray<UA_String, opcua::Type::String>(discoveryUrls.size());
+    for (size_t i = 0; i < discoveryUrls.size(); ++i) {
+        TypeConverter<std::string>::toNative(discoveryUrls[i], handle()->discoveryUrls[i]);
+    }
+}
 }  // namespace opcua
