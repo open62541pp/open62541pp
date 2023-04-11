6bfb88231cdd625fb693e517e09b91680ff99512
Eduardo Lopes <elopes1@oceaneering.com>
2023-04-11 09:01:54 -0300

    new test cases
    Signed-off-by: Eduardo Lopes <elopes1@oceaneering.com>

--- tests/Client.cpp
+++ tests/Client.cpp
@@ -6,7 +6,7 @@
 
 #include "open62541pp/Helper.h"
 #include "open62541pp/Node.h"
-#include "open62541pp/NodeId.h"
+#include "open62541pp/types/NodeId.h"
 #include "open62541pp/Server.h"
 #include "open62541pp/Client.h"
 
@@ -44,8 +44,37 @@ TEST_CASE("Client") {
 	SECTION("List Endpoints")
 	{
 		Client client;
-
 		auto servers = client.findServers("opc.tcp://localhost:4840");		
 		REQUIRE(servers.size() >= 1);
 	}
+        SECTION("Get Endpoints")
+        {
+                Client client;
+                auto endpoints = client.getEndpoints("opc.tcp://localhost:4840");
+                REQUIRE(endpoints.size() >= 1);
+        }
+
+        SECTION("Client::connect")
+        {
+                Client client;
+                REQUIRE_NOTHROW(client.connect("opc.tcp://localhost:4840"));
+        }
+
+        SECTION("Client::readValueAttributes")
+        {
+                Client client;
+                REQUIRE_NOTHROW(client.connect("opc.tcp://localhost:4840"));
+                    // clang-format off
+        auto rootFolder = client.readValueAttribute(NodeId{0, UA_NS0ID_ROOTFOLDER});
+        auto objFolder = client.readValueAttribute(NodeId{0, UA_NS0ID_OBJECTSFOLDER});
+        auto typesFolder = client.readValueAttribute(NodeId{0, UA_NS0ID_TYPESFOLDER});
+        auto viewsFolder = client.readValueAttribute(NodeId{0, UA_NS0ID_VIEWSFOLDER});
+        auto objTypesFolder = client.readValueAttribute(NodeId{0, UA_NS0ID_OBJECTTYPESFOLDER});
+        auto varTypesFolder = client.readValueAttribute(NodeId{0, UA_NS0ID_VARIABLETYPESFOLDER});
+        auto dataTypesFolder = client.readValueAttribute(NodeId{0, UA_NS0ID_DATATYPESFOLDER});
+        auto refTypesFolder = client.readValueAttribute(NodeId{0, UA_NS0ID_REFERENCETYPESFOLDER});
+        auto baseObjType = client.readValueAttribute(NodeId{0, UA_NS0ID_BASEOBJECTTYPE});
+        auto baseDataVariableType = client.readValueAttribute(NodeId{0, UA_NS0ID_BASEDATAVARIABLETYPE});
+        }
+
 }
