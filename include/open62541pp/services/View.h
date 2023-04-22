a34b10a7b1296712045d3a264c81cfee7d8899b2
Lukas Berbuer <lukas.berbuer@gmail.com>
2023-04-22 09:10:20 +0200

    feat: implement browse, browseNext for server

--- include/open62541pp/services/View.h
+++ include/open62541pp/services/View.h
@@ -4,6 +4,7 @@
 
 #include "open62541pp/Common.h"
 #include "open62541pp/types/Builtin.h"
+#include "open62541pp/types/Composed.h"
 #include "open62541pp/types/NodeId.h"
 
 // forward declarations
@@ -18,6 +19,23 @@ namespace opcua::services {
  * Browse the address space / view created by a server.
  * @ingroup Services
  */
+
+/**
+ * Discover the references of a specified node.
+ * @see https://reference.opcfoundation.org/Core/Part4/v105/docs/5.8.2
+ * @ingroup View
+ */
+template <typename T>
+BrowseResult browse(T& serverOrClient, const BrowseDescription& bd, uint32_t maxReferences = 0);
+
+/**
+ * Request the next step of browse or browseNext response.
+ * The response might get split up if the information is too large to be sent in a single response.
+ * @see https://reference.opcfoundation.org/Core/Part4/v105/docs/5.8.3
+ * @ingroup View
+ */
+template <typename T>
+BrowseResult browseNext(T& serverOrClient, const ByteString& continuationPoint);
 
 /**
  * Get a child specified by its path from this node (only local nodes).
--- src/services/View.cpp
+++ src/services/View.cpp
@@ -7,6 +7,20 @@
 
 namespace opcua::services {
 
+template <>
+BrowseResult browse<Server>(Server& server, const BrowseDescription& bd, uint32_t maxReferences) {
+    BrowseResult result = UA_Server_browse(server.handle(), maxReferences, bd.handle());
+    detail::throwOnBadStatus(result->statusCode);
+    return result;
+}
+
+template <>
+BrowseResult browseNext<Server>(Server& server, const ByteString& continuationPoint) {
+    BrowseResult result = UA_Server_browseNext(server.handle(), false, continuationPoint.handle());
+    detail::throwOnBadStatus(result->statusCode);
+    return result;
+}
+
 NodeId browseChild(Server& server, const NodeId& origin, const std::vector<QualifiedName>& path) {
     const std::vector<UA_QualifiedName> pathNative(path.begin(), path.end());
     const TypeWrapper<UA_BrowsePathResult, UA_TYPES_BROWSEPATHRESULT> result(
