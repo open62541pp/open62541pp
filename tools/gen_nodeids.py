400e06d2cdb5445601a7991604bc7bb8490e3399
Lukas Berbuer <lukas.berbuer@gmail.com>
2023-04-28 23:37:52 +0200

    feat: generated nodeids header

---
+++ tools/gen_nodeids.py
@@ -0,0 +1,76 @@
+import urllib.request
+from collections import defaultdict
+from pathlib import Path
+
+URL = "http://www.opcfoundation.org/UA/schemas/1.04/NodeIds.csv"
+HERE = Path(__file__).parent
+HEADER_FILE = HERE.parent / "include" / "open62541pp" / "NodeIds.h"
+
+TEMPLATE_HEADER = """
+/* ---------------------------------------------------------------------------------------------- */
+/*                                   Generated - do not modify!                                   */
+/* ---------------------------------------------------------------------------------------------- */
+
+#pragma once
+
+namespace opcua {{
+
+// clang-format off
+
+{body}
+
+// clang-format on
+
+}}  // namespace opcua
+""".lstrip()
+
+TEMPLATE_ENUM = """
+/**
+ * {nodeclass} node ids defined by the OPC UA specification (generated).
+ * @see https://reference.opcfoundation.org/Core/Part6/v105/docs/A.3
+ */
+enum class {enum_name} : uint32_t {{
+{body}
+}};
+""".strip()
+
+
+def main():
+    opener = urllib.request.build_opener()
+    opener.addheaders = [("User-agent", "Mozilla/5.0")]
+
+    with opener.open(URL) as f:
+        content = f.read().decode("utf-8")
+
+    nodeids = defaultdict(list)  # node class as key
+
+    for line in content.splitlines():
+        name, id, nodeclass = line.split(",")
+        nodeids[nodeclass].append((name, int(id)))
+
+    def gen_enum_definition(enum_name: str, nodeclass: str) -> str:
+        body = "\n".join(f"    {name} = {id}," for name, id in nodeids[nodeclass])
+        return TEMPLATE_ENUM.format(
+            nodeclass=nodeclass,
+            enum_name=enum_name,
+            body=body,
+        )
+
+    enums = [
+        gen_enum_definition(f"{nodeclass}Id", nodeclass)
+        for nodeclass in (
+            "DataType",
+            "ReferenceType",
+            "ObjectType",
+            "VariableType",
+            "Object",
+            "Variable",
+            "Method",
+        )
+    ]
+    header = TEMPLATE_HEADER.format(body="\n\n".join(enums))
+    HEADER_FILE.write_text(header)
+
+
+if __name__ == "__main__":
+    main()
