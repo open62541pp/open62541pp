db247cafb3d0f489871fec398c5b5ffcf6811d52
Lukas Berbuer <36054362+lukasberbuer@users.noreply.github.com>
2023-05-10 22:06:22 +0200

    fix: amalgamation support (#47)
    Fix support for open62541 with amalgamated header and source files as reported in issue #46.
    Amalgamation is enabled with the `UA_ENABLE_AMALGAMATION` CMake option.

--- include/open62541pp/open62541.h
+++ include/open62541pp/open62541.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// public open62541 headers needed by open62541++
+
 #ifndef _MSC_VER
 // ignore compile warnings of open62541 v1.3:
 // - missing initializer for member ‘UA_NodeId::identifier’
@@ -9,10 +11,15 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-// public open62541 headers needed by open62541++
+#if __has_include(<open62541.h>)
+// UA_ENABLE_AMALGAMATION=ON
+#include <open62541.h>
+#else
+// UA_ENABLE_AMALGAMATION=OFF
 #include <open62541/nodeids.h>
 #include <open62541/types.h>
 #include <open62541/types_generated.h>
+#endif
 
 #ifndef _MSC_VER
 #pragma GCC diagnostic pop
