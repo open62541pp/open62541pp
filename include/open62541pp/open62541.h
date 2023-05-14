609d0b2d5afde76b758f869ac3209ced7145f8e5
Lukas Berbuer <lukas.berbuer@gmail.com>
2023-05-14 21:30:55 +0200

    build: ignore open62541 compile warning with clang

--- include/open62541pp/open62541.h
+++ include/open62541pp/open62541.h
@@ -3,12 +3,15 @@
 // public open62541 headers needed by open62541++
 
 #ifndef _MSC_VER
-// ignore compile warnings of open62541 v1.3:
+// ignore compile warnings of open62541:
 // - missing initializer for member ‘UA_NodeId::identifier’
 // - missing initializer for member ‘UA_ExpandedNodeId::namespaceUri’
 // - missing initializer for member ‘UA_ExpandedNodeId::serverIndex’
+// - suggest braces around initialization of subobject [-Wmissing-braces]
+//   UA_ExpandedNodeId id = {0}; id.nodeId = nodeId; return id;
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wmissing-braces"
 #endif
 
 #if __has_include(<open62541.h>)
