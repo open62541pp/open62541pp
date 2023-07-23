5e5fe11ecb2b17e9860313cd51ac2f9625d1f559
Lukas Berbuer <lukas.berbuer@gmail.com>
2023-07-23 17:09:27 +0200

    test: encrypted server/client connection

--- tests/Crypto.cpp
+++ tests/Crypto.cpp
@@ -51,4 +51,58 @@ TEST_CASE("Create certificate") {
     }
 }
 
+TEST_CASE("Encrypted connection server/client") {
+    const std::string serverApplicationUri = "urn:open62541.server.application";  // default
+    const std::string clientApplicationUri = "urn:unconfigured:application";  // default
+    const size_t keySizeBits = 2048;
+
+    // create server certificate
+    const auto certServer = crypto::createCertificate(
+        {
+            String{"C=DE"},
+            String{"O=open62541pp"},
+            String{"CN=open62541ppServer@localhost"},
+        },
+        {
+            String{"DNS:localhost"},
+            String{std::string("URI:").append(serverApplicationUri)},
+        },
+        keySizeBits
+    );
+
+    // create client certificate
+    const auto certClient = crypto::createCertificate(
+        {
+            String{"C=DE"},
+            String{"O=open62541pp"},
+            String{"CN=open62541ppClient@localhost"},
+        },
+        {
+            String{"DNS:localhost"},
+            String{std::string("URI:").append(clientApplicationUri)},
+        },
+        keySizeBits
+    );
+
+    SUBCASE("Connect without trusting each others certificate") {
+        Server server(4840, certServer.certificate, certServer.privateKey, {}, {}, {});
+        ServerRunner serverRunner(server);
+
+        Client client(certClient.certificate, certClient.privateKey, {}, {});
+        client.setSecurityMode(MessageSecurityMode::SignAndEncrypt);
+        CHECK_THROWS(client.connect("opc.tcp://localhost:4840"));
+    }
+
+    SUBCASE("Connect with trust lists") {
+        Server server(
+            4840, certServer.certificate, certServer.privateKey, {certClient.certificate}, {}, {}
+        );
+        ServerRunner serverRunner(server);
+
+        Client client(certClient.certificate, certClient.privateKey, {certServer.certificate}, {});
+        client.setSecurityMode(MessageSecurityMode::SignAndEncrypt);
+        CHECK_NOTHROW(client.connect("opc.tcp://localhost:4840"));
+    }
+}
+
 #endif
