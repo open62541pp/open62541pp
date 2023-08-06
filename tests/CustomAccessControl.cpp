f73b0257221124a3f1fbf0b3a6617701820379e8
Lukas Berbuer <lukas.berbuer@gmail.com>
2023-08-06 20:09:34 +0200

    test: reset custom access control

--- tests/CustomAccessControl.cpp
+++ tests/CustomAccessControl.cpp
@@ -36,183 +36,203 @@ TEST_CASE("CustomAccessControl") {
     CustomAccessControl customAccessControl(server, native);
     AccessControlTest accessControl;
 
-    customAccessControl.setAccessControl(accessControl);
+    SUBCASE("Set non-existing access control") {
+        CHECK_NOTHROW(customAccessControl.setAccessControl());
+        CHECK(native.context == nullptr);
+    }
 
-    CHECK(native.context != nullptr);
+    SUBCASE("Set custom access control") {
+        CHECK_NOTHROW(customAccessControl.setAccessControl(accessControl));
+        CHECK(native.context != nullptr);
 
-    CHECK(native.userTokenPoliciesSize == 1);  // anonymous only
-    CHECK(native.userTokenPolicies != nullptr);
-    CHECK(native.userTokenPolicies[0].tokenType == UA_USERTOKENTYPE_ANONYMOUS);  // NOLINT
+        CHECK(native.userTokenPoliciesSize == 1);  // anonymous only
+        CHECK(native.userTokenPolicies != nullptr);
+        CHECK(native.userTokenPolicies[0].tokenType == UA_USERTOKENTYPE_ANONYMOUS);  // NOLINT
 
-    CHECK(native.activateSession != nullptr);
-    CHECK(
-        native.activateSession(
+        CHECK(native.activateSession != nullptr);
+        CHECK(
+            native.activateSession(
+                server.handle(),
+                &native,
+                nullptr,  // endpoint description
+                nullptr,  // secure channel remote certificate
+                nullptr,  // session id
+                nullptr,  // user identity token
+                nullptr  // session context
+            ) == UA_STATUSCODE_GOOD
+        );
+
+        CHECK(native.closeSession != nullptr);
+        CHECK_NOTHROW(native.closeSession(
             server.handle(),
             &native,
-            nullptr,  // endpoint description
-            nullptr,  // secure channel remote certificate
             nullptr,  // session id
-            nullptr,  // user identity token
             nullptr  // session context
-        ) == UA_STATUSCODE_GOOD
-    );
+        ));
 
-    CHECK(native.closeSession != nullptr);
-    CHECK_NOTHROW(native.closeSession(
-        server.handle(),
-        &native,
-        nullptr,  // session id
-        nullptr  // session context
-    ));
+        CHECK(native.getUserRightsMask != nullptr);
+        CHECK(
+            native.getUserRightsMask(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr,  // node id
+                nullptr  // node context
+            ) == 0xFFFFFFFF
+        );
 
-    CHECK(native.getUserRightsMask != nullptr);
-    CHECK(
-        native.getUserRightsMask(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr,  // node id
-            nullptr  // node context
-        ) == 0xFFFFFFFF
-    );
+        CHECK(native.getUserAccessLevel != nullptr);
+        CHECK(
+            native.getUserAccessLevel(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr,  // node id
+                nullptr  // node context
+            ) == 0xFF
+        );
 
-    CHECK(native.getUserAccessLevel != nullptr);
-    CHECK(
-        native.getUserAccessLevel(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr,  // node id
-            nullptr  // node context
-        ) == 0xFF
-    );
+        CHECK(native.getUserExecutable != nullptr);
+        CHECK(
+            native.getUserExecutable(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr,  // method id
+                nullptr  // method context
+            ) == true
+        );
 
-    CHECK(native.getUserExecutable != nullptr);
-    CHECK(
-        native.getUserExecutable(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr,  // method id
-            nullptr  // method context
-        ) == true
-    );
+        CHECK(native.getUserExecutableOnObject != nullptr);
+        CHECK(
+            native.getUserExecutableOnObject(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr,  // method id
+                nullptr,  // method context
+                nullptr,  // object id
+                nullptr  // object context
+            ) == true
+        );
 
-    CHECK(native.getUserExecutableOnObject != nullptr);
-    CHECK(
-        native.getUserExecutableOnObject(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr,  // method id
-            nullptr,  // method context
-            nullptr,  // object id
-            nullptr  // object context
-        ) == true
-    );
+        CHECK(native.allowAddNode != nullptr);
+        CHECK(
+            native.allowAddNode(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr  // item
+            ) == true
+        );
 
-    CHECK(native.allowAddNode != nullptr);
-    CHECK(
-        native.allowAddNode(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr  // item
-        ) == true
-    );
+        CHECK(native.allowAddReference != nullptr);
+        CHECK(
+            native.allowAddReference(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr  // item
+            ) == true
+        );
 
-    CHECK(native.allowAddReference != nullptr);
-    CHECK(
-        native.allowAddReference(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr  // item
-        ) == true
-    );
+        CHECK(native.allowDeleteNode != nullptr);
+        CHECK(
+            native.allowDeleteNode(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr  // item
+            ) == false
+        );
 
-    CHECK(native.allowDeleteNode != nullptr);
-    CHECK(
-        native.allowDeleteNode(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr  // item
-        ) == false
-    );
-
-    CHECK(native.allowDeleteReference != nullptr);
-    CHECK(
-        native.allowDeleteReference(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr  // item
-        ) == false
-    );
+        CHECK(native.allowDeleteReference != nullptr);
+        CHECK(
+            native.allowDeleteReference(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr  // item
+            ) == false
+        );
 
 #if UAPP_OPEN62541_VER_GE(1, 1)
-    CHECK(native.allowBrowseNode != nullptr);
-    CHECK(
-        native.allowBrowseNode(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr,  // node id
-            nullptr  // node context
-        ) == true
-    );
+        CHECK(native.allowBrowseNode != nullptr);
+        CHECK(
+            native.allowBrowseNode(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr,  // node id
+                nullptr  // node context
+            ) == true
+        );
 #endif
 
 #if UAPP_OPEN62541_VER_GE(1, 2) && defined(UA_ENABLE_SUBSCRIPTIONS)
-    CHECK(native.allowTransferSubscription != nullptr);
-    CHECK(
-        native.allowTransferSubscription(
-            server.handle(),
-            &native,
-            nullptr,  // old session id
-            nullptr,  // old session context
-            nullptr,  // new session id
-            nullptr  // new session context
-        ) == true
-    );
+        CHECK(native.allowTransferSubscription != nullptr);
+        CHECK(
+            native.allowTransferSubscription(
+                server.handle(),
+                &native,
+                nullptr,  // old session id
+                nullptr,  // old session context
+                nullptr,  // new session id
+                nullptr  // new session context
+            ) == true
+        );
 #endif
 
 #ifdef UA_ENABLE_HISTORIZING
-    CHECK(native.allowHistoryUpdateUpdateData != nullptr);
-    CHECK(
-        native.allowHistoryUpdateUpdateData(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr,  // node id
-            UA_PERFORMUPDATETYPE_INSERT,
-            nullptr  // value
-        ) == true
-    );
+        CHECK(native.allowHistoryUpdateUpdateData != nullptr);
+        CHECK(
+            native.allowHistoryUpdateUpdateData(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr,  // node id
+                UA_PERFORMUPDATETYPE_INSERT,
+                nullptr  // value
+            ) == true
+        );
 
-    CHECK(native.allowHistoryUpdateDeleteRawModified != nullptr);
-    CHECK(
-        native.allowHistoryUpdateDeleteRawModified(
-            server.handle(),
-            &native,
-            nullptr,  // session id
-            nullptr,  // session context
-            nullptr,  // node id
-            {},  // start timestamp,
-            {},  // stop timestamp,
-            true
-        ) == true
-    );
+        CHECK(native.allowHistoryUpdateDeleteRawModified != nullptr);
+        CHECK(
+            native.allowHistoryUpdateDeleteRawModified(
+                server.handle(),
+                &native,
+                nullptr,  // session id
+                nullptr,  // session context
+                nullptr,  // node id
+                {},  // start timestamp,
+                {},  // stop timestamp,
+                true
+            ) == true
+        );
 #endif
+    }
+
+    SUBCASE("Reset custom access control") {
+        CHECK_NOTHROW(customAccessControl.setAccessControl(accessControl));
+        CHECK(native.context != nullptr);
+        auto* oldContext = native.context;
+
+        // manipulate native object
+        native.context = nullptr;
+
+        // reset should re-populate native object
+        CHECK_NOTHROW(customAccessControl.setAccessControl());
+        CHECK(native.context != nullptr);
+        CHECK(native.context == oldContext);
+    }
 }
