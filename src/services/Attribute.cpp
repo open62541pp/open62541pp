efaaac52063714a17ef43f1200cf92557ffe500a
Lukas Berbuer <lukas.berbuer@gmail.com>
2023-11-01 20:05:51 +0100

    refactor: generic client service in attribute service set

--- src/services/Attribute.cpp
+++ src/services/Attribute.cpp
@@ -6,23 +6,37 @@
 #include "open62541pp/TypeWrapper.h"  // asWrapper, asNative
 
 #include "../open62541_impl.h"
+#include "ClientService.h"
 
 namespace opcua::services {
 
 ReadResponse read(Client& client, const ReadRequest& request) {
-    ReadResponse response = UA_Client_Service_read(client.handle(), request);
-    detail::throwOnBadStatus(response->responseHeader.serviceResult);
-    return response;
+    return sendRequest<UA_ReadRequest, UA_ReadResponse>(
+        client, request, ForwardResponse<UA_ReadResponse>{}
+    );
+}
+
+inline static auto createReadValueId(const NodeId& id, AttributeId attributeId) {
+    UA_ReadValueId item{};
+    item.nodeId = *id.handle();
+    item.attributeId = static_cast<uint32_t>(attributeId);
+    return item;
+}
+
+inline static auto createReadRequest(
+    TimestampsToReturn timestamps, Span<const ReadValueId> nodesToRead
+) noexcept {
+    UA_ReadRequest request{};
+    request.timestampsToReturn = static_cast<UA_TimestampsToReturn>(timestamps);
+    request.nodesToReadSize = nodesToRead.size();
+    request.nodesToRead = asNative(const_cast<ReadValueId*>(nodesToRead.data()));  // NOLINT
+    return request;
 }
 
 ReadResponse read(
     Client& client, Span<const ReadValueId> nodesToRead, TimestampsToReturn timestamps
 ) {
-    // avoid copy of nodesToRead
-    UA_ReadRequest request{};
-    request.timestampsToReturn = static_cast<UA_TimestampsToReturn>(timestamps);
-    request.nodesToReadSize = nodesToRead.size();
-    request.nodesToRead = asNative(const_cast<ReadValueId*>(nodesToRead.data()));  // NOLINT
+    const auto request = createReadRequest(timestamps, nodesToRead);
     return read(client, asWrapper<ReadRequest>(request));
 }
 
@@ -30,10 +44,7 @@ template <>
 DataValue readAttribute<Server>(
     Server& server, const NodeId& id, AttributeId attributeId, TimestampsToReturn timestamps
 ) {
-    UA_ReadValueId item{};
-    item.nodeId = *id.handle();
-    item.attributeId = static_cast<uint32_t>(attributeId);
-
+    const auto item = createReadValueId(id, attributeId);
     DataValue result = UA_Server_read(
         server.handle(), &item, static_cast<UA_TimestampsToReturn>(timestamps)
     );
@@ -47,7 +58,9 @@ template <>
 DataValue readAttribute<Client>(
     Client& client, const NodeId& id, AttributeId attributeId, TimestampsToReturn timestamps
 ) {
-    auto response = read(client, {{id, attributeId}}, timestamps);
+    const auto item = createReadValueId(id, attributeId);
+    const auto request = createReadRequest(timestamps, {asWrapper<ReadValueId>(&item), 1});
+    auto response = read(client, asWrapper<ReadRequest>(request));
     auto results = response.getResults();
     if (results.size() != 1 || !results[0]->hasValue) {
         throw BadStatus(UA_STATUSCODE_BADUNEXPECTEDERROR);
@@ -61,9 +74,9 @@ DataValue readAttribute<Client>(
 }
 
 WriteResponse write(Client& client, const WriteRequest& request) {
-    WriteResponse response = UA_Client_Service_write(client.handle(), request);
-    detail::throwOnBadStatus(response->responseHeader.serviceResult);
-    return response;
+    return sendRequest<UA_WriteRequest, UA_WriteResponse>(
+        client, request, ForwardResponse<UA_WriteResponse>{}
+    );
 }
 
 WriteResponse write(Client& client, Span<const WriteValue> nodesToWrite) {
@@ -74,17 +87,22 @@ WriteResponse write(Client& client, Span<const WriteValue> nodesToWrite) {
     return write(client, asWrapper<WriteRequest>(request));
 }
 
+inline static UA_WriteValue createWriteValue(
+    const NodeId& id, AttributeId attributeId, const DataValue& value
+) noexcept {
+    UA_WriteValue item{};
+    item.nodeId = *id.handle();
+    item.attributeId = static_cast<uint32_t>(attributeId);
+    item.value = *value.handle();  // shallow copy, avoid copy of value
+    item.value.hasValue = true;
+    return item;
+}
+
 template <>
 void writeAttribute<Server>(
     Server& server, const NodeId& id, AttributeId attributeId, const DataValue& value
 ) {
-    // avoid copy of value
-    UA_WriteValue item{};
-    item.nodeId = *id.handle();
-    item.attributeId = static_cast<uint32_t>(attributeId);
-    item.value = *value.handle();  // shallow copy
-    item.value.hasValue = true;
-
+    const auto item = createWriteValue(id, attributeId, value);
     const auto status = UA_Server_write(server.handle(), &item);
     detail::throwOnBadStatus(status);
 }
@@ -93,13 +111,7 @@ template <>
 void writeAttribute<Client>(
     Client& client, const NodeId& id, AttributeId attributeId, const DataValue& value
 ) {
-    // avoid copy of value
-    UA_WriteValue item{};
-    item.nodeId = *id.handle();
-    item.attributeId = static_cast<uint32_t>(attributeId);
-    item.value = *value.handle();  // shallow copy
-    item.value.hasValue = true;
-
+    const auto item = createWriteValue(id, attributeId, value);
     auto response = write(client, {asWrapper<WriteValue>(&item), 1});
     auto results = response.getResults();
     if (results.size() != 1) {
