f01f11a17370597708b44b31dd081b1f9c400d62
Lukas Berbuer <lukas.berbuer@gmail.com>
2023-11-08 22:45:52 +0100

    fix: remove stdout debug

--- src/CustomLogger.h
+++ src/CustomLogger.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>
-
 #include "open62541pp/Logger.h"
 
 #include "open62541_impl.h"  // UA_Logger
@@ -11,10 +9,6 @@ namespace opcua {
 class CustomLogger {
 public:
     explicit CustomLogger(UA_Logger& logger);
-
-    ~CustomLogger() {
-        std::cout << "Shutdown!\n";
-    }
 
     void setLogger(Logger logger);
     const Logger& getLogger() const noexcept;
