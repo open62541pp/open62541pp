103e262404b62a9e254581c296d639c6a0ec3d18
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-01-16 23:09:33 +0100

    fix: win/macos build

--- tests/BitMask.cpp
+++ tests/BitMask.cpp
@@ -7,98 +7,94 @@
 
 using namespace opcua;
 
-namespace testenv {
 using Underlying = uint8_t;
 
-enum class Access : Underlying {
-    Read = 1,
-    Write = 2,
+enum class BitMaskEnum : Underlying {
+    One = 1,
+    Two = 2,
 };
-}  // namespace testenv
 
 namespace opcua {
 template <>
-struct IsBitMaskEnum<testenv::Access> : std::true_type {};
+struct IsBitMaskEnum<BitMaskEnum> : std::true_type {};
 }  // namespace opcua
 
 TEST_CASE("Bitwise operations with enum (enabled with IsBitMaskEnum trait)") {
-    using namespace testenv;
-
-    CHECK(IsBitMaskEnum<Access>::value == true);
+    CHECK(IsBitMaskEnum<BitMaskEnum>::value == true);
 
     SUBCASE("AND") {
-        CHECK(static_cast<Underlying>(Access::Read & Access::Read) == 1);
-        CHECK(static_cast<Underlying>(Access::Read & Access::Write) == 0);
+        CHECK(static_cast<Underlying>(BitMaskEnum::One & BitMaskEnum::One) == 1);
+        CHECK(static_cast<Underlying>(BitMaskEnum::One & BitMaskEnum::Two) == 0);
     }
 
     SUBCASE("OR") {
-        CHECK(static_cast<Underlying>(Access::Read | Access::Read) == 1);
-        CHECK(static_cast<Underlying>(Access::Read | Access::Write) == 3);
+        CHECK(static_cast<Underlying>(BitMaskEnum::One | BitMaskEnum::One) == 1);
+        CHECK(static_cast<Underlying>(BitMaskEnum::One | BitMaskEnum::Two) == 3);
     }
 
     SUBCASE("XOR") {
-        CHECK(static_cast<Underlying>(Access::Read ^ Access::Read) == 0);
-        CHECK(static_cast<Underlying>(Access::Read ^ Access::Write) == 3);
+        CHECK(static_cast<Underlying>(BitMaskEnum::One ^ BitMaskEnum::One) == 0);
+        CHECK(static_cast<Underlying>(BitMaskEnum::One ^ BitMaskEnum::Two) == 3);
     }
 
     SUBCASE("NOT") {
-        CHECK(static_cast<Underlying>(~Access::Read) == std::numeric_limits<Underlying>::max() - 1);
+        CHECK(
+            static_cast<Underlying>(~BitMaskEnum::One) == std::numeric_limits<Underlying>::max() - 1
+        );
     }
 
     SUBCASE("AND assignment") {
-        Access mask{};
-        mask = Access::Read;
-        mask &= Access::Read;
+        BitMaskEnum mask{};
+        mask = BitMaskEnum::One;
+        mask &= BitMaskEnum::One;
         CHECK(static_cast<Underlying>(mask) == 1);
 
-        mask = Access::Read;
-        mask &= Access::Write;
+        mask = BitMaskEnum::One;
+        mask &= BitMaskEnum::Two;
         CHECK(static_cast<Underlying>(mask) == 0);
     }
 
     SUBCASE("OR assignment") {
-        Access mask{};
-        mask = Access::Read;
-        mask |= Access::Read;
+        BitMaskEnum mask{};
+        mask = BitMaskEnum::One;
+        mask |= BitMaskEnum::One;
         CHECK(static_cast<Underlying>(mask) == 1);
 
-        mask = Access::Read;
-        mask |= Access::Write;
+        mask = BitMaskEnum::One;
+        mask |= BitMaskEnum::Two;
         CHECK(static_cast<Underlying>(mask) == 3);
     }
 
     SUBCASE("XOR assignment") {
-        Access mask{};
-        mask = Access::Read;
-        mask ^= Access::Read;
+        BitMaskEnum mask{};
+        mask = BitMaskEnum::One;
+        mask ^= BitMaskEnum::One;
         CHECK(static_cast<Underlying>(mask) == 0);
 
-        mask = Access::Read;
-        mask ^= Access::Write;
+        mask = BitMaskEnum::One;
+        mask ^= BitMaskEnum::Two;
         CHECK(static_cast<Underlying>(mask) == 3);
     }
 }
 
 TEST_CASE("BitMask") {
-    using namespace testenv;
-
     SUBCASE("Constructors") {
-        CHECK_NOTHROW(BitMask<Access>());
-        CHECK_NOTHROW(BitMask<Access>(Access::Read));
-        CHECK_NOTHROW(BitMask<Access>(2));
+        CHECK_NOTHROW(BitMask<BitMaskEnum>());
+        CHECK_NOTHROW(BitMask<BitMaskEnum>(BitMaskEnum::One));
+        CHECK_NOTHROW(BitMask<BitMaskEnum>(2));
     }
 
     SUBCASE("Conversion to enum") {
-        CHECK(static_cast<Access>(BitMask<Access>(Access::Read)) == Access::Read);
+        CHECK(static_cast<BitMaskEnum>(BitMask<BitMaskEnum>(BitMaskEnum::One)) == BitMaskEnum::One);
     }
 
     SUBCASE("Conversion to underlying") {
-        // CHECK(static_cast<Underlying>(BitMask<Access>(2)) == 2);  // deprecated
+        // CHECK(static_cast<Underlying>(BitMask<BitMaskEnum>(2)) == 2);  // deprecated
     }
 
     SUBCASE("get()") {
-        CHECK(BitMask<Access>().get() == 0);
-        CHECK(BitMask<Access>(Access::Read).get() == 1);
-        CHECK(BitMask<Access>(Access::Write).get() == 2);
+        CHECK(BitMask<BitMaskEnum>().get() == 0);
+        CHECK(BitMask<BitMaskEnum>(BitMaskEnum::One).get() == 1);
+        CHECK(BitMask<BitMaskEnum>(BitMaskEnum::Two).get() == 2);
     }
 }
