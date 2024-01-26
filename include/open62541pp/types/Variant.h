96eaf4fbda7566e9e8f7faddfe284ff6ddb3610d
Lukas Berbuer <36054362+lukasberbuer@users.noreply.github.com>
2024-01-26 23:36:25 +0100

    perf: ref-qualified Variant::getScalar (#176)

--- include/open62541pp/types/Variant.h
+++ include/open62541pp/types/Variant.h
@@ -33,7 +33,6 @@ private:
     using EnableIfNoSpan = typename std::enable_if_t<!detail::IsSpan<T>::value>;
 
 public:
-    // NOLINTNEXTLINE, false positive?
     using TypeWrapperBase::TypeWrapperBase;  // inherit constructors
 
     /// Create Variant from scalar value (no copy if assignable without conversion).
@@ -185,18 +184,29 @@ public:
     /// Get reference to scalar value with given template type (only native or wrapper types).
     /// @exception BadVariantAccess If the variant is not a scalar or not of type `T`.
     template <typename T>
-    T& getScalar() {
+    T& getScalar() & {
         return const_cast<T&>(std::as_const(*this).getScalar<T>());  // NOLINT
     }
 
-    /// Get const reference to scalar value with given template type (only native or wrapper types).
-    /// @exception BadVariantAccess If the variant is not a scalar or not of type `T`.
+    /// @copydoc getScalar()&
     template <typename T>
-    const T& getScalar() const {
+    const T& getScalar() const& {
         assertIsNative<T>();
         checkIsScalar();
         checkIsDataType<T>();
         return *static_cast<const T*>(handle()->data);
+    }
+
+    /// @copydoc getScalar()&
+    template <typename T>
+    T&& getScalar() && {
+        return std::move(getScalar<T>());
+    }
+
+    /// @copydoc getScalar()&
+    template <typename T>
+    const T&& getScalar() const&& {
+        return std::move(getScalar<T>());
     }
 
     /// Get copy of scalar value with given template type.
