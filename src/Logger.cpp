492950ea7d5f44ab748afbb9e642044986e312bb
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-02-26 08:10:20 +0100

    refactor: use helper functions in log implementation

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -10,33 +10,33 @@
 
 namespace opcua {
 
-inline static const UA_Logger& getLogger(UA_Client& client) {
-    return UA_Client_getConfig(&client)->logger;
+inline static const UA_Logger* getLogger(UA_Client* client) noexcept {
+    return client == nullptr ? nullptr : &detail::getConfig(client)->logger;
 }
 
-inline static const UA_Logger& getLogger(UA_Server& server) {
-    return UA_Server_getConfig(&server)->logger;
+inline static const UA_Logger* getLogger(UA_Server* server) noexcept {
+    return server == nullptr ? nullptr : &detail::getConfig(server)->logger;
 }
 
-inline static const UA_Logger& getLogger(Client& client) {
-    return getLogger(*client.handle());
+inline static const UA_Logger* getLogger(Client* client) noexcept {
+    return getLogger(client->handle());
 }
 
-inline static const UA_Logger& getLogger(Server& server) {
-    return getLogger(*server.handle());
+inline static const UA_Logger* getLogger(Server* server) noexcept {
+    return getLogger(server->handle());
 }
 
 template <typename T>
 inline static void logImpl(
-    T& serverOrClient, LogLevel level, LogCategory category, std::string_view msg
+    T* serverOrClient, LogLevel level, LogCategory category, std::string_view msg
 ) {
-    const auto& logger = getLogger(serverOrClient);
-    if (logger.log == nullptr) {
+    const auto* logger = getLogger(serverOrClient);
+    if (logger == nullptr || logger->log == nullptr) {
         return;
     }
     va_list args{};  // NOLINT
-    logger.log(
-        logger.context,
+    logger->log(
+        logger->context,
         static_cast<UA_LogLevel>(level),
         static_cast<UA_LogCategory>(category),
         std::string(msg).c_str(),
@@ -45,23 +45,19 @@ inline static void logImpl(
 }
 
 void log(UA_Client* client, LogLevel level, LogCategory category, std::string_view msg) {
-    if (client != nullptr) {
-        logImpl(*client, level, category, msg);
-    }
-}
-
-void log(Client& client, LogLevel level, LogCategory category, std::string_view msg) {
     logImpl(client, level, category, msg);
 }
 
+void log(Client& client, LogLevel level, LogCategory category, std::string_view msg) {
+    logImpl(&client, level, category, msg);
+}
+
 void log(UA_Server* server, LogLevel level, LogCategory category, std::string_view msg) {
-    if (server != nullptr) {
-        logImpl(*server, level, category, msg);
-    }
+    logImpl(server, level, category, msg);
 }
 
 void log(Server& server, LogLevel level, LogCategory category, std::string_view msg) {
-    logImpl(server, level, category, msg);
+    logImpl(&server, level, category, msg);
 }
 
 }  // namespace opcua
