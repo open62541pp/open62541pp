f3d61ff59671f5e337369406c267cb868c848e32
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-02-28 05:53:15 +0100

    test: pass nullptr to log function

--- tests/Logger.cpp
+++ tests/Logger.cpp
@@ -11,11 +11,13 @@ using namespace opcua;
 TEST_CASE_TEMPLATE("Log with custom logger", T, Server, Client) {
     T serverOrClient;
 
+    static size_t counter = 0;
     static LogLevel lastLogLevel{};
     static LogCategory lastLogCategory{};
     static std::string lastMessage{};
 
     serverOrClient.setLogger([&](LogLevel level, LogCategory category, std::string_view message) {
+        counter++;
         lastLogLevel = level;
         lastLogCategory = category;
         lastMessage = message;
@@ -25,12 +27,18 @@ TEST_CASE_TEMPLATE("Log with custom logger", T, Server, Client) {
     serverOrClient.setLogger(nullptr);
 
     log(serverOrClient, LogLevel::Info, LogCategory::Server, "Message");
+    CHECK(counter == 1);
     CHECK(lastLogLevel == LogLevel::Info);
     CHECK(lastLogCategory == LogCategory::Server);
     CHECK(lastMessage == "Message");
 
     log(serverOrClient.handle(), LogLevel::Warning, LogCategory::Server, "Message from native");
+    CHECK(counter == 2);
     CHECK(lastLogLevel == LogLevel::Warning);
     CHECK(lastLogCategory == LogCategory::Server);
     CHECK(lastMessage == "Message from native");
+
+    using NativePtr = decltype(serverOrClient.handle());
+    log(NativePtr{nullptr}, LogLevel::Warning, LogCategory::Server, "Message from null");
+    CHECK(counter == 2);
 }
