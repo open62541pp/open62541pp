ebb686696feef23b6d8789643ca997894e44e9cc
Lukas Berbuer <36054362+lukasberbuer@users.noreply.github.com>
2024-03-15 16:19:15 +0100

    refactor: copy & move NodeId in ExpandedNodeId constructor (#233)

--- include/open62541pp/types/NodeId.h
+++ include/open62541pp/types/NodeId.h
@@ -166,8 +166,8 @@ public:
     // NOLINTNEXTLINE, false positive?
     using TypeWrapperBase::TypeWrapperBase;  // inherit constructors
 
-    explicit ExpandedNodeId(const NodeId& id);
-    ExpandedNodeId(const NodeId& id, std::string_view namespaceUri, uint32_t serverIndex);
+    explicit ExpandedNodeId(NodeId id) noexcept;
+    ExpandedNodeId(NodeId id, std::string_view namespaceUri, uint32_t serverIndex);
 
     bool isLocal() const noexcept {
         return handle()->serverIndex == 0;
