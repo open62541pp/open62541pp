74935da1d5cbfa92fab1b28ebf6d96ada6e4fae9
lukasberbuer <lukasberbuer@users.noreply.github.com>
2024-07-18 20:02:05 +0000

    deploy: 2c9be20d37f3583aba942c5e3320cef08ae668d9

--- search/classes_c.js
+++ search/classes_c.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['node_0',['Node',['../classopcua_1_1Node.html',1,'opcua']]],
-  ['nodeattributes_1',['NodeAttributes',['../classopcua_1_1NodeAttributes.html',1,'opcua']]],
-  ['nodecontext_2',['NodeContext',['../structopcua_1_1detail_1_1NodeContext.html',1,'opcua::detail']]],
-  ['nodeid_3',['NodeId',['../classopcua_1_1NodeId.html',1,'opcua']]],
-  ['numericrange_4',['NumericRange',['../classopcua_1_1NumericRange.html',1,'opcua']]]
+  ['namespace_0',['Namespace',['../structopcua_1_1Namespace.html',1,'opcua']]],
+  ['node_1',['Node',['../classopcua_1_1Node.html',1,'opcua']]],
+  ['nodeattributes_2',['NodeAttributes',['../classopcua_1_1NodeAttributes.html',1,'opcua']]],
+  ['nodecontext_3',['NodeContext',['../structopcua_1_1detail_1_1NodeContext.html',1,'opcua::detail']]],
+  ['nodeid_4',['NodeId',['../classopcua_1_1NodeId.html',1,'opcua']]],
+  ['numericrange_5',['NumericRange',['../classopcua_1_1NumericRange.html',1,'opcua']]]
 ];
