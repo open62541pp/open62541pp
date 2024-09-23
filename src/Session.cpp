5d45486c788e2735f17aa6c13c208414725719ea
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-09-23 15:41:47 +0200

    feat: implement Session member functions for v1.4

--- src/Session.cpp
+++ src/Session.cpp
@@ -14,17 +14,20 @@
 namespace opcua {
 
 // ignore namespace index for v1.3, v1.4 uses qualified keys
-[[maybe_unused]] inline static std::string unqualifiedKey(const QualifiedName& key) {
+[[maybe_unused]] inline static std::string unqualify(const QualifiedName& key) {
     return std::string{key.getName()};
 }
 
 Variant Session::getSessionAttribute([[maybe_unused]] const QualifiedName& key) {
     Variant variant;
 #if UAPP_OPEN62541_VER_EQ(1, 3)
-    const auto status = UA_Server_getSessionParameter(
-        connection().handle(), id().handle(), unqualifiedKey(key).c_str(), variant.handle()
-    );
-    throwIfBad(status);
+    throwIfBad(UA_Server_getSessionParameter(
+        connection().handle(), id().handle(), unqualify(key).c_str(), variant.handle()
+    ));
+#elif UAPP_OPEN62541_VER_GE(1, 4)
+    throwIfBad(UA_Server_getSessionAttributeCopy(
+        connection().handle(), id().handle(), key, variant.handle()
+    ));
 #endif
     return variant;
 }
@@ -33,25 +36,27 @@ void Session::setSessionAttribute(
     [[maybe_unused]] const QualifiedName& key, [[maybe_unused]] const Variant& value
 ) {
 #if UAPP_OPEN62541_VER_EQ(1, 3)
-    const auto status = UA_Server_setSessionParameter(
-        connection().handle(), id().handle(), unqualifiedKey(key).c_str(), value.handle()
+    throwIfBad(UA_Server_setSessionParameter(
+        connection().handle(), id().handle(), unqualify(key).c_str(), value.handle()
+    ));
+#elif UAPP_OPEN62541_VER_GE(1, 4)
+    throwIfBad(
+        UA_Server_setSessionAttribute(connection().handle(), id().handle(), key, value.handle())
     );
-    throwIfBad(status);
 #endif
 }
 
 void Session::deleteSessionAttribute([[maybe_unused]] const QualifiedName& key) {
 #if UAPP_OPEN62541_VER_EQ(1, 3)
-    UA_Server_deleteSessionParameter(
-        connection().handle(), id().handle(), unqualifiedKey(key).c_str()
-    );
+    UA_Server_deleteSessionParameter(connection().handle(), id().handle(), unqualify(key).c_str());
+#elif UAPP_OPEN62541_VER_GE(1, 4)
+    throwIfBad(UA_Server_deleteSessionAttribute(connection().handle(), id().handle(), key));
 #endif
 }
 
 void Session::close() {
-#if UAPP_OPEN62541_VER_EQ(1, 3)
-    const auto status = UA_Server_closeSession(connection().handle(), id().handle());
-    throwIfBad(status);
+#if UAPP_OPEN62541_VER_GE(1, 3)
+    throwIfBad(UA_Server_closeSession(connection().handle(), id().handle()));
 #endif
 }
 
