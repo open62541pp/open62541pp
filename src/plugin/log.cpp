a6650df8329adab771cf447744b0c4abcf47d07f
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-09-26 22:10:30 +0200

    refactor: LoggerBase class for custom logger implementations (future)

--- src/plugin/log.cpp
+++ src/plugin/log.cpp
@@ -3,14 +3,63 @@
 #include <cstdarg>  // va_list
 #include <string>
 
+#include "open62541pp/config.hpp"
 #include "open62541pp/client.hpp"
 #include "open62541pp/config.hpp"
-#include "open62541pp/detail/open62541/common.h"
+#include "open62541pp/detail/string_utils.hpp"  // detail::toString
 #include "open62541pp/server.hpp"
 
 namespace opcua {
 
-static void logImpl(UA_Logger* logger, LogLevel level, LogCategory category, std::string_view msg) {
+static void logNative(
+    void* context, UA_LogLevel level, UA_LogCategory category, const char* msg, va_list args
+) {
+    assert(context != nullptr);
+    static_cast<LoggerBase*>(context)->log(
+        static_cast<LogLevel>(level),
+        static_cast<LogCategory>(category),
+        detail::toString(msg, args)
+    );
+}
+
+UA_Logger LoggerBase::create() {
+    UA_Logger native{};
+    native.log = logNative;
+    native.context = this;
+#if UAPP_OPEN62541_VER_GE(1, 4)
+    native.clear = [](UA_Logger* ptr) { UA_free(ptr); };
+#endif
+    return native;
+}
+
+void LoggerBase::clear(UA_Logger& native) noexcept {
+    if (native.clear != nullptr) {
+#if UAPP_OPEN62541_VER_GE(1, 4)
+        // TODO:
+        // Open62541 v1.4 transitioned to pointers for UA_Logger instances.
+        // The clear function doesn't clear the context anymore but frees the memory and
+        // consequently invalidates pointers like UA_EventLoop.logger.
+        // Neighter the open62541 loggers UA_Log_Syslog_log, UA_Log_Syslog_log, nor the
+        // opcua::Logger needs to be cleared, so skip this for now.
+
+        // native.clear(&native);
+#else
+        native.clear(native.context);
+        native.context = nullptr;
+#endif
+    }
+}
+
+// void LoggerBase::clear(UA_Logger*& plugin) noexcept override {
+//     if (plugin != nullptr) {
+//         clear(*plugin);
+//         plugin = nullptr;
+//     }
+// }
+
+/* ---------------------------------------------------------------------------------------------- */
+
+void log(const UA_Logger* logger, LogLevel level, LogCategory category, std::string_view msg) {
     if (logger == nullptr || logger->log == nullptr) {
         return;
     }
@@ -22,6 +71,10 @@ static void logImpl(UA_Logger* logger, LogLevel level, LogCategory category, std
         std::string(msg).c_str(),
         args  // NOLINT
     );
+}
+
+void log(const UA_Logger& logger, LogLevel level, LogCategory category, std::string_view msg) {
+    log(&logger, level, category, msg);
 }
 
 template <typename T>
@@ -38,7 +91,7 @@ static UA_Logger* getLogger(T* connection) noexcept {
 }
 
 void log(UA_Client* client, LogLevel level, LogCategory category, std::string_view msg) {
-    logImpl(getLogger(client), level, category, msg);
+    log(getLogger(client), level, category, msg);
 }
 
 void log(Client& client, LogLevel level, LogCategory category, std::string_view msg) {
@@ -46,7 +99,7 @@ void log(Client& client, LogLevel level, LogCategory category, std::string_view 
 }
 
 void log(UA_Server* server, LogLevel level, LogCategory category, std::string_view msg) {
-    logImpl(getLogger(server), level, category, msg);
+    log(getLogger(server), level, category, msg);
 }
 
 void log(Server& server, LogLevel level, LogCategory category, std::string_view msg) {
---
+++ tests/plugin_log.cpp
@@ -0,0 +1,61 @@
+#include <string>
+
+#include <doctest/doctest.h>
+
+#include "open62541pp/plugin/log.hpp"
+
+using namespace opcua;
+
+class LoggerTest : public LoggerBase {
+public:
+    void log(LogLevel level, LogCategory category, std::string_view msg) override {
+        lastLevel = level;
+        lastCategory = category;
+        lastMessage = msg;
+    }
+
+    LogLevel lastLevel;
+    LogCategory lastCategory;
+    std::string lastMessage;
+};
+
+TEST_CASE("LoggerBase") {
+    LoggerTest logger;
+    auto native = logger.create();
+    
+    CHECK(native.context == &logger);
+    CHECK(native.log != nullptr);
+
+    va_list args{};
+    native.log(native.context, UA_LOGLEVEL_INFO, UA_LOGCATEGORY_USERLAND, "message", args);
+
+    CHECK(logger.lastLevel == LogLevel::Info);
+    CHECK(logger.lastCategory == LogCategory::Userland);
+    CHECK(logger.lastMessage == "message");
+
+    logger.clear(native);
+}
+
+TEST_CASE("log") {
+    struct LogContext {
+        UA_LogLevel level;
+        UA_LogCategory category;
+        std::string message;
+    };
+
+    UA_Logger logger{};
+    LogContext context;
+    logger.context = &context;
+    logger.log =
+        [](void* logContext, UA_LogLevel level, UA_LogCategory category, const char* msg, va_list) {
+            CHECK(logContext != nullptr);
+            static_cast<LogContext*>(logContext)->level = level;
+            static_cast<LogContext*>(logContext)->category = category;
+            static_cast<LogContext*>(logContext)->message = msg;
+        };
+
+    log(logger, LogLevel::Info, LogCategory::Userland, "message");
+    CHECK(context.level == UA_LOGLEVEL_INFO);
+    CHECK(context.category == UA_LOGCATEGORY_USERLAND);
+    CHECK(context.message == "message");
+}
