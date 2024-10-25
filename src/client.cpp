3ab58d6306aebfd22d15497176fefad0e1e0c711
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-10-25 16:29:12 +0200

    feat: asNative overloads for Client*/Server*

--- src/client.cpp
+++ src/client.cpp
@@ -522,6 +522,10 @@ Client* asWrapper(UA_Client* client) noexcept {
     return static_cast<Client*>(config->clientContext);
 }
 
+UA_Client* asNative(Client* client) noexcept {
+    return client == nullptr ? nullptr : client->handle();
+}
+
 /* -------------------------------------- Helper functions -------------------------------------- */
 
 namespace detail {
