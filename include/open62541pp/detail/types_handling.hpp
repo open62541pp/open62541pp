00efbbf8a644aa2bce3051f7fbf9caac7b525e79
Lukas Berbuer <36054362+lukasberbuer@users.noreply.github.com>
2024-10-29 11:09:05 +0100

    refactor: types handling (#414)

--- include/open62541pp/detail/types_handling.hpp
+++ include/open62541pp/detail/types_handling.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>  // copy_n
 #include <cassert>
+#include <cstring>  // memcpy, memset
 #include <memory>
 #include <new>  // bad_alloc
 
@@ -32,6 +33,23 @@ constexpr bool isPointerFree = IsOneOf<  // NOLINT(modernize-type-traits)
     UA_StatusCode>::value;
 
 template <typename T>
+constexpr bool isBorrowed(const T& /* unused */) noexcept {
+    return false;
+}
+
+constexpr bool isBorrowed(const UA_Variant& native) noexcept {
+    return native.storageType == UA_VARIANT_DATA_NODELETE;
+}
+
+constexpr bool isBorrowed(const UA_DataValue& native) noexcept {
+    return native.value.storageType == UA_VARIANT_DATA_NODELETE;
+}
+
+constexpr bool isBorrowed(const UA_ExtensionObject& native) noexcept {
+    return native.encoding == UA_EXTENSIONOBJECT_DECODED_NODELETE;
+}
+
+template <typename T>
 constexpr bool isValidTypeCombination(const UA_DataType& type) {
     if constexpr (std::is_void_v<T>) {
         return true;  // allow type-erasure
@@ -43,30 +61,12 @@ constexpr bool isValidTypeCombination(const UA_DataType& type) {
 template <typename T>
 constexpr void clear(T& native, const UA_DataType& type) noexcept {
     assert(isValidTypeCombination<T>(type));
-    if constexpr (!isPointerFree<T>) {
-        UA_clear(&native, &type);
-    }
-}
-
-// Overloads for types that allow borrowing
-
-constexpr void clear(UA_Variant& native, const UA_DataType& type) noexcept {
-    assert(isValidTypeCombination<UA_Variant>(type));
-    if (native.storageType != UA_VARIANT_DATA_NODELETE) {
-        UA_clear(&native, &type);
-    }
-}
-
-constexpr void clear(UA_DataValue& native, const UA_DataType& type) noexcept {
-    assert(isValidTypeCombination<UA_DataValue>(type));
-    if (native.value.storageType != UA_VARIANT_DATA_NODELETE) {
-        UA_clear(&native, &type);
-    }
-}
-
-constexpr void clear(UA_ExtensionObject& native, const UA_DataType& type) noexcept {
-    assert(isValidTypeCombination<UA_ExtensionObject>(type));
-    if (native.encoding != UA_EXTENSIONOBJECT_DECODED_NODELETE) {
+    // NOLINTNEXTLINE(bugprone-branch-clone)
+    if constexpr (isPointerFree<T>) {
+        native = {};
+    } else if (isBorrowed(native)) {
+        native = {};
+    } else {
         UA_clear(&native, &type);
     }
 }
@@ -131,17 +131,29 @@ template <typename T>
 
 template <typename T>
 [[nodiscard]] T* copyArray(const T* src, size_t size, const UA_DataType& type) {
-    assert(isValidTypeCombination<T>(type));
+    T* dst = allocateArray<T>(size, type);
     if constexpr (!isPointerFree<T>) {
-        T* dst{};
-        throwIfBad(UA_Array_copy(src, size, (void**)&dst, &type));  // NOLINT
-        return dst;
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        std::transform(src, src + size, dst, [&](const T& item) { return copy(item, type); });
     } else {
-        T* dst = allocateArray<T>(size, type);
         std::copy_n(src, size, dst);
-        return dst;
     }
+    return dst;
+}
+
+template <typename T>
+void resizeArray(T*& array, size_t& size, size_t newSize, const UA_DataType& type) {
+    if (newSize == size) {
+        return;
+    }
+    T* newArray = allocateArray<T>(newSize, type);
+    std::memcpy(newArray, array, std::min(size, newSize) * sizeof(T));
+    if (newSize > size) {
+        std::memset(newArray + size, 0, newSize - size);  // NOLINT
+    }
+    deallocateArray<T>(array, size, type);
+    array = newArray;
+    size = newSize;
 }
 
 }  // namespace opcua::detail
- 
--- tests/types_handling.cpp
+++ tests/types_handling.cpp
@@ -6,43 +6,111 @@
 
 using namespace opcua;
 
-TEST_CASE("isPointerFree") {
-    CHECK(detail::isPointerFree<bool>);
-    CHECK(detail::isPointerFree<int>);
-    CHECK(detail::isPointerFree<float>);
-    CHECK(detail::isPointerFree<UA_Guid>);
-    CHECK_FALSE(detail::isPointerFree<UA_String>);
-    CHECK_FALSE(detail::isPointerFree<UA_NodeId>);
-}
+TEST_CASE("Types handling") {
+    SUBCASE("isPointerFree") {
+        CHECK(detail::isPointerFree<bool>);
+        CHECK(detail::isPointerFree<int>);
+        CHECK(detail::isPointerFree<float>);
+        CHECK(detail::isPointerFree<UA_Guid>);
+        CHECK_FALSE(detail::isPointerFree<UA_String>);
+        CHECK_FALSE(detail::isPointerFree<UA_NodeId>);
+    }
 
-TEST_CASE("Allocate / deallocate") {
-    auto* ptr = detail::allocate<UA_String>(UA_TYPES[UA_TYPES_STRING]);
-    CHECK(ptr != nullptr);
-    detail::deallocate(ptr, UA_TYPES[UA_TYPES_STRING]);
-}
+    SUBCASE("Allocate / deallocate") {
+        auto* ptr = detail::allocate<UA_String>(UA_TYPES[UA_TYPES_STRING]);
+        CHECK(ptr != nullptr);
+        detail::deallocate(ptr, UA_TYPES[UA_TYPES_STRING]);
+    }
 
-TEST_CASE("Allocate as unique_ptr") {
-    auto ptr = detail::allocateUniquePtr<UA_String>(UA_TYPES[UA_TYPES_STRING]);
-    CHECK(ptr.get() != nullptr);
-}
-
-TEST_CASE("Allocate / deallocate array") {
-    auto* ptr = detail::allocateArray<UA_String>(3, UA_TYPES[UA_TYPES_STRING]);
-    CHECK(ptr != nullptr);
-    detail::deallocateArray(ptr, 3, UA_TYPES[UA_TYPES_STRING]);
-
-    SUBCASE("Exceed memory") {
-        CHECK_THROWS_AS(
-            []() {
-                const auto huge = size_t(-1);
-                return detail::allocateArray<UA_String>(huge, UA_TYPES[UA_TYPES_STRING]);
-            }(),
-            std::bad_alloc
-        );
+    SUBCASE("Allocate as unique_ptr") {
+        auto ptr = detail::allocateUniquePtr<UA_String>(UA_TYPES[UA_TYPES_STRING]);
+        CHECK(ptr.get() != nullptr);
     }
 }
 
-TEST_CASE("Allocate array as unique_ptr") {
-    auto ptr = detail::allocateArrayUniquePtr<UA_String>(3, UA_TYPES[UA_TYPES_STRING]);
-    CHECK(ptr.get() != nullptr);
+TEST_CASE("Array handling") {
+    SUBCASE("Allocate / deallocate") {
+        auto* ptr = detail::allocateArray<UA_String>(3, UA_TYPES[UA_TYPES_STRING]);
+        CHECK(ptr != nullptr);
+        detail::deallocateArray(ptr, 3, UA_TYPES[UA_TYPES_STRING]);
+
+        SUBCASE("Exceed memory") {
+            CHECK_THROWS_AS(
+                []() {
+                    const auto huge = size_t(-1);
+                    return detail::allocateArray<UA_String>(huge, UA_TYPES[UA_TYPES_STRING]);
+                }(),
+                std::bad_alloc
+            );
+        }
+    }
+
+    SUBCASE("Allocate as unique_ptr") {
+        auto ptr = detail::allocateArrayUniquePtr<UA_String>(3, UA_TYPES[UA_TYPES_STRING]);
+        CHECK(ptr.get() != nullptr);
+    }
+
+    SUBCASE("Copy") {
+        SUBCASE("Without pointers") {
+            const size_t size = 2;
+            const auto& type = UA_TYPES[UA_TYPES_INT32];
+            auto* src = detail::allocateArray<UA_Int32>(size, type);
+            src[0] = 1;
+            src[1] = 2;
+
+            auto* dst = detail::copyArray(src, size, type);
+            CHECK(dst[0] == 1);
+            CHECK(dst[1] == 2);
+
+            detail::deallocateArray(src, size, type);
+            detail::deallocateArray(dst, size, type);
+        }
+
+        SUBCASE("With pointers") {
+            const size_t size = 2;
+            const auto& type = UA_TYPES[UA_TYPES_STRING];
+            auto* src = detail::allocateArray<UA_String>(size, type);
+            src[0] = UA_STRING_ALLOC("one");
+            src[1] = UA_STRING_ALLOC("two");
+
+            auto* dst = detail::copyArray(src, size, type);
+            CHECK(UA_String_equal(&dst[0], &src[0]));
+            CHECK(UA_String_equal(&dst[1], &src[1]));
+
+            detail::deallocateArray(src, size, type);
+            detail::deallocateArray(dst, size, type);
+        }
+    }
+
+    SUBCASE("Resize array") {
+        size_t size = 3;
+        const auto& type = UA_TYPES[UA_TYPES_INT32];
+        auto* array = detail::allocateArray<UA_Int32>(size, type);
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+
+        const auto* arrayBefore = array;
+
+        SUBCASE("newSize > size") {
+            detail::resizeArray(array, size, 4, type);
+            CHECK(size == 4);
+            CHECK(array[0] == 1);
+            CHECK(array[1] == 2);
+            CHECK(array[2] == 3);
+            CHECK(array[3] == 0);
+        }
+
+        SUBCASE("newSize < size") {
+            detail::resizeArray(array, size, 2, type);
+            CHECK(size == 2);
+            CHECK(array[0] == 1);
+            CHECK(array[1] == 2);
+        }
+
+        CHECK(array != nullptr);
+        CHECK(array != arrayBefore);
+
+        detail::deallocateArray(array, size, type);
+    }
 }
