ca61fe554fca84a9d114f15b095d61c3ef88fc16
Lukas Berbuer <36054362+lukasberbuer@users.noreply.github.com>
2024-11-04 10:14:56 +0100

    refactor: simplify attribute handler (#425)

--- include/open62541pp/services/detail/attribute_handler.hpp
+++ include/open62541pp/services/detail/attribute_handler.hpp
@@ -1,15 +1,14 @@
 #pragma once
 
+#include <cassert>
 #include <cstdint>
 #include <type_traits>
-#include <utility>  // exchange, move
+#include <utility>  // forward, move
 #include <vector>
 
 #include "open62541pp/bitmask.hpp"
 #include "open62541pp/common.hpp"  // AttributeId, WriteMask, EventNotifier, AccessLevel
 #include "open62541pp/detail/open62541/common.h"
-#include "open62541pp/detail/result_utils.hpp"
-#include "open62541pp/exception.hpp"
 #include "open62541pp/types.hpp"
 
 namespace opcua::services::detail {
@@ -22,22 +21,6 @@ inline Result<Variant> getVariant(DataValue&& dv) noexcept {
         return BadResult(UA_STATUSCODE_BADUNEXPECTEDERROR);
     }
     return std::move(dv).getValue();
-}
-
-template <typename T>
-Result<T> tryGetScalar(Variant&& var) noexcept {
-    return opcua::detail::tryInvoke([&] { return std::move(var).getScalar<T>(); });
-}
-
-template <typename T>
-Result<std::vector<T>> tryGetArray(Variant&& var) noexcept {
-    return opcua::detail::tryInvoke([&] { return std::move(var).getArrayCopy<T>(); });
-}
-
-inline DataValue createDataValueFromStatus(StatusCode code) noexcept {
-    DataValue dv;
-    dv.setStatus(code);
-    return dv;
 }
 
 /**
@@ -68,16 +51,16 @@ struct AttributeHandlerScalar {
     using Type = T;
 
     static Result<Type> fromDataValue(DataValue&& dv) noexcept {
-        return getVariant(std::move(dv)).andThen(tryGetScalar<T>);
+        return getVariant(std::move(dv)).transform([](Variant&& var) {
+            assert(var.isType<T>());
+            assert(var.isScalar());
+            return std::move(var).getScalar<T>();
+        });
     }
 
     template <typename U>
-    static DataValue toDataValue(U&& value) noexcept {
-        return opcua::detail::tryInvoke([&] {
-                   return DataValue::fromScalar(std::forward<U>(value));
-               })
-            .orElse(createDataValueFromStatus)
-            .value();
+    static DataValue toDataValue(U&& value) {
+        return DataValue::fromScalar(std::forward<U>(value));
     }
 };
 
@@ -93,7 +76,7 @@ struct AttributeHandlerScalar<T, std::enable_if_t<std::is_enum_v<T>>> {
         });
     }
 
-    static DataValue toDataValue(Type value) noexcept {
+    static DataValue toDataValue(Type value) {
         return UnderlyingHandler::toDataValue(static_cast<UnderlyingType>(value));
     }
 };
@@ -110,7 +93,7 @@ struct AttributeHandlerScalar<Bitmask<T>> {
         });
     }
 
-    static DataValue toDataValue(Type value) noexcept {
+    static DataValue toDataValue(Type value) {
         return UnderlyingHandler::toDataValue(value.get());
     }
 };
@@ -175,13 +158,15 @@ struct AttributeHandler<AttributeId::ArrayDimensions> {
     using Type = std::vector<uint32_t>;
 
     static Result<Type> fromDataValue(DataValue&& dv) noexcept {
-        return getVariant(std::move(dv)).andThen(tryGetArray<uint32_t>);
+        return getVariant(std::move(dv)).transform([](Variant&& var) {
+            assert(var.isType<uint32_t>());
+            assert(var.isArray());
+            return std::move(var).getArrayCopy<uint32_t>();
+        });
     }
 
-    static DataValue toDataValue(Span<const uint32_t> dimensions) noexcept {
-        return opcua::detail::tryInvoke([&] { return DataValue::fromArray(dimensions); })
-            .orElse(createDataValueFromStatus)
-            .value();
+    static DataValue toDataValue(Span<const uint32_t> dimensions) {
+        return DataValue::fromArray(dimensions);
     }
 };
 
