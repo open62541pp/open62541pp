561b0f07874c4500e7e4b5c1734a662ca981f7be
lukasberbuer <lukasberbuer@users.noreply.github.com>
2024-11-14 18:59:49 +0000

    deploy: 39d919cfd74bf8f71e38b60d6ff7847997070367

--- search/functions_a.js
+++ search/functions_a.js
@@ -2,13 +2,16 @@ var searchData=
 [
   ['methodattributes_0',['MethodAttributes',['../classopcua_1_1MethodAttributes.html#a3881c7513fca73537233b1c59bdfd3f5',1,'opcua::MethodAttributes']]],
   ['modifymonitoreditem_1',['modifyMonitoredItem',['../group__ModifyMonitoredItems.html#ga8e2f797476b706e5b4dc4d04ae2796b7',1,'opcua::services']]],
-  ['modifymonitoreditems_2',['modifyMonitoredItems',['../group__ModifyMonitoredItems.html#ga0606e899369b87b21f5f5c9ad5a4b7fb',1,'opcua::services']]],
-  ['modifymonitoreditemsrequest_3',['ModifyMonitoredItemsRequest',['../classopcua_1_1ModifyMonitoredItemsRequest.html#acb91afeedc9c1ba312625378dfa6a60c',1,'opcua::ModifyMonitoredItemsRequest']]],
-  ['modifysubscription_4',['modifySubscription',['../group__ModifySubscription.html#ga1157f9d4cae2e7487c0212d3951fe2a1',1,'opcua::services::modifySubscription(Client &amp;connection, const ModifySubscriptionRequest &amp;request) noexcept'],['../group__ModifySubscription.html#ga8f9e4ce11c22da0d3e38df0c1bb2b1d1',1,'opcua::services::modifySubscription(Client &amp;connection, uint32_t subscriptionId, const SubscriptionParameters &amp;parameters) noexcept']]],
-  ['modifysubscriptionrequest_5',['ModifySubscriptionRequest',['../classopcua_1_1ModifySubscriptionRequest.html#a36594c082a85e0fc069169511fb99d2d',1,'opcua::ModifySubscriptionRequest']]],
-  ['monitoreditem_6',['MonitoredItem',['../classopcua_1_1MonitoredItem.html#acb419ec306a251435b000851d2e60c2f',1,'opcua::MonitoredItem']]],
-  ['monitoreditemcreaterequest_7',['MonitoredItemCreateRequest',['../classopcua_1_1MonitoredItemCreateRequest.html#a1bfcceb3adfd8e398357c17fac3ae7b8',1,'opcua::MonitoredItemCreateRequest']]],
-  ['monitoreditemid_8',['monitoredItemId',['../classopcua_1_1MonitoredItem.html#a9f644f6f2b3186d67b60e7cd0fe92bbb',1,'opcua::MonitoredItem']]],
-  ['monitoreditemmodifyrequest_9',['MonitoredItemModifyRequest',['../classopcua_1_1MonitoredItemModifyRequest.html#ac60a613064886f5a1209151d3fc33e1e',1,'opcua::MonitoredItemModifyRequest']]],
-  ['monitoringparameters_10',['MonitoringParameters',['../classopcua_1_1MonitoringParameters.html#a431b5e40fe472d15a84d279fe4fa899e',1,'opcua::MonitoringParameters']]]
+  ['modifymonitoreditemasync_2',['modifyMonitoredItemAsync',['../group__ModifyMonitoredItems.html#ga60d775d245334e39d5346314946578c6',1,'opcua::services']]],
+  ['modifymonitoreditems_3',['modifyMonitoredItems',['../group__ModifyMonitoredItems.html#ga0606e899369b87b21f5f5c9ad5a4b7fb',1,'opcua::services']]],
+  ['modifymonitoreditemsasync_4',['modifyMonitoredItemsAsync',['../group__ModifyMonitoredItems.html#ga860f92d817460fc027bfd50a917b7701',1,'opcua::services']]],
+  ['modifymonitoreditemsrequest_5',['ModifyMonitoredItemsRequest',['../classopcua_1_1ModifyMonitoredItemsRequest.html#acb91afeedc9c1ba312625378dfa6a60c',1,'opcua::ModifyMonitoredItemsRequest']]],
+  ['modifysubscription_6',['modifySubscription',['../group__ModifySubscription.html#ga1157f9d4cae2e7487c0212d3951fe2a1',1,'opcua::services::modifySubscription(Client &amp;connection, const ModifySubscriptionRequest &amp;request) noexcept'],['../group__ModifySubscription.html#ga8f9e4ce11c22da0d3e38df0c1bb2b1d1',1,'opcua::services::modifySubscription(Client &amp;connection, uint32_t subscriptionId, const SubscriptionParameters &amp;parameters) noexcept']]],
+  ['modifysubscriptionasync_7',['modifySubscriptionAsync',['../group__ModifySubscription.html#gad909b444442c40f3d37e0e74a8baba6e',1,'opcua::services::modifySubscriptionAsync(Client &amp;connection, const ModifySubscriptionRequest &amp;request, CompletionToken &amp;&amp;token)'],['../group__ModifySubscription.html#gabde9e1135a3cb60c63de495b2c02dd9f',1,'opcua::services::modifySubscriptionAsync(Client &amp;connection, uint32_t subscriptionId, const SubscriptionParameters &amp;parameters, CompletionToken &amp;&amp;token)']]],
+  ['modifysubscriptionrequest_8',['ModifySubscriptionRequest',['../classopcua_1_1ModifySubscriptionRequest.html#a36594c082a85e0fc069169511fb99d2d',1,'opcua::ModifySubscriptionRequest']]],
+  ['monitoreditem_9',['MonitoredItem',['../classopcua_1_1MonitoredItem.html#acb419ec306a251435b000851d2e60c2f',1,'opcua::MonitoredItem']]],
+  ['monitoreditemcreaterequest_10',['MonitoredItemCreateRequest',['../classopcua_1_1MonitoredItemCreateRequest.html#a1bfcceb3adfd8e398357c17fac3ae7b8',1,'opcua::MonitoredItemCreateRequest']]],
+  ['monitoreditemid_11',['monitoredItemId',['../classopcua_1_1MonitoredItem.html#a9f644f6f2b3186d67b60e7cd0fe92bbb',1,'opcua::MonitoredItem']]],
+  ['monitoreditemmodifyrequest_12',['MonitoredItemModifyRequest',['../classopcua_1_1MonitoredItemModifyRequest.html#ac60a613064886f5a1209151d3fc33e1e',1,'opcua::MonitoredItemModifyRequest']]],
+  ['monitoringparameters_13',['MonitoringParameters',['../classopcua_1_1MonitoringParameters.html#a431b5e40fe472d15a84d279fe4fa899e',1,'opcua::MonitoringParameters']]]
 ];
