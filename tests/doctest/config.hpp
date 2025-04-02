58592f87b4e28c72a0b655e82b6b1cbbdc66b1a2
Lukas Berbuer <lukas.berbuer@gmail.com>
2025-04-02 14:29:42 +0200

    test: remove doctest workarounds

--- tests/doctest/config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/**
- * Doctest configuration.
- * This include file is automatically included by CMake before any other includes.
- */
-
-// avoid calling opcua::toString through ADL
-#define DOCTEST_STRINGIFY(...) ::doctest::toString(__VA_ARGS__)
