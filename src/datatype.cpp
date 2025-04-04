d1e41ea82a68395af83eba030b1ee309779336f8
Lukas Berbuer <36054362+lukasberbuer@users.noreply.github.com>
2025-04-04 13:18:34 +0200

    feat: DataTypeMember wrapper (#564)

--- src/datatype.cpp
+++ src/datatype.cpp
@@ -10,62 +10,6 @@ namespace opcua {
 
 namespace detail {
 
-DataTypeMember createDataTypeMember(
-    [[maybe_unused]] const char* memberName,
-    const UA_DataType& memberType,
-    uint8_t padding,
-    bool isArray,
-    [[maybe_unused]] bool isOptional
-) noexcept {
-    DataTypeMember result{};
-#if UAPP_HAS_TYPEDESCRIPTION
-    result.memberName = memberName;
-#endif
-#if UAPP_OPEN62541_VER_GE(1, 3)
-    result.memberType = &memberType;
-#else
-    result.memberTypeIndex = memberType.typeIndex;
-    result.namespaceZero = memberType.typeId.namespaceIndex == 0;
-#endif
-    result.padding = padding;
-    result.isArray = isArray;  // NOLINT
-#if UAPP_OPEN62541_VER_GE(1, 1)
-    result.isOptional = isOptional;  // NOLINT
-#endif
-    return result;
-}
-
-UA_DataType createDataType(
-    [[maybe_unused]] const char* typeName,
-    UA_NodeId typeId,
-    UA_NodeId binaryEncodingId,
-    uint16_t memSize,
-    uint8_t typeKind,
-    bool pointerFree,
-    bool overlayable,
-    uint32_t membersSize,
-    DataTypeMember* members
-) noexcept {
-    UA_DataType result{};
-#if UAPP_HAS_TYPEDESCRIPTION
-    result.typeName = typeName;
-#endif
-    result.typeId = typeId;
-#if UAPP_OPEN62541_VER_GE(1, 2)
-    result.binaryEncodingId = binaryEncodingId;
-#else
-    assert(binaryEncodingId.identifierType == UA_NODEIDTYPE_NUMERIC);
-    result.binaryEncodingId = binaryEncodingId.identifier.numeric;  // NOLINT
-#endif
-    result.memSize = memSize;
-    result.typeKind = typeKind;
-    result.pointerFree = pointerFree;
-    result.overlayable = overlayable;
-    result.membersSize = membersSize;
-    result.members = members;
-    return result;
-}
-
 UA_DataTypeArray createDataTypeArray(
     const UA_DataType* types, size_t typesSize, const UA_DataTypeArray* next
 ) noexcept {
@@ -131,10 +75,42 @@ UA_DataType copy(const UA_DataType& src) {
 
 }  // namespace detail
 
+DataTypeMember::DataTypeMember(const UA_DataTypeMember& native)
+    : Wrapper(detail::copy(native)) {}
+
+DataTypeMember::DataTypeMember(UA_DataTypeMember&& native) noexcept  // NOLINT
+    : Wrapper(std::exchange(native, {})) {}
+
+DataTypeMember::~DataTypeMember() {
+    detail::clear(native());
+}
+
+DataTypeMember::DataTypeMember(const DataTypeMember& other)
+    : Wrapper(detail::copy(other.native())) {}
+
+DataTypeMember::DataTypeMember(DataTypeMember&& other) noexcept
+    : Wrapper(std::exchange(other.native(), {})) {}
+
+DataTypeMember& DataTypeMember::operator=(const DataTypeMember& other) {
+    if (this != &other) {
+        detail::clear(native());
+        native() = detail::copy(other.native());
+    }
+    return *this;
+}
+
+DataTypeMember& DataTypeMember::operator=(DataTypeMember&& other) noexcept {
+    if (this != &other) {
+        detail::clear(native());
+        native() = std::exchange(other.native(), {});
+    }
+    return *this;
+}
+
 DataType::DataType(const UA_DataType& native)
     : Wrapper(detail::copy(native)) {}
 
-DataType::DataType(UA_DataType&& native)  // NOLINT
+DataType::DataType(UA_DataType&& native) noexcept  // NOLINT
     : Wrapper(std::exchange(native, {})) {}
 
 DataType::DataType(TypeIndex typeIndex)
@@ -171,7 +147,7 @@ DataType& DataType::operator=(DataType&& other) noexcept {
 void DataType::setMembers(Span<const DataTypeMember> members) {
     assert(members.size() < (1U << 8U));
     detail::clearMembers(native());
-    detail::copyMembers(members.data(), members.size(), native());
+    detail::copyMembers(asNative(members.data()), members.size(), native());
 }
 
 }  // namespace opcua
