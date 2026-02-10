8b5be6da5488947e684185d950587d22acd7f8e1
Lukas Berbuer <lukas.berbuer@gmail.com>
2026-02-10 22:04:57 +0100

    fix: use backwards compatible alias

--- src/services_nodemanagement.cpp
+++ src/services_nodemanagement.cpp
@@ -196,7 +196,7 @@ StatusCode setMethodCallback<Server>(
     return opcua::detail::tryInvoke([&] {
         auto nodeContext = std::make_unique<opcua::detail::NodeContext>();
         nodeContext->methodCallback = std::move(callback);
-        throwIfBad(UA_Server_setMethodNodeCallback(connection.handle(), id, methodCallback));
+        throwIfBad(UA_Server_setMethodNode_callback(connection.handle(), id, methodCallback));
         throwIfBad(UA_Server_setNodeContext(connection.handle(), id, nodeContext.get()));
         opcua::detail::getContext(connection).nodeContexts.insert(id, std::move(nodeContext));
     }).code();
